10bd5ac2f4f9d7ec864b232a2ae637bfa5b025f2
Zaher Ghaibeh <zaher@zah.me>
2020-09-02 16:29:40 +0300

    adding password confirmation to 2FA using livewire stack

--- src/Http/Livewire/TwoFactorAuthenticationForm.php
+++ src/Http/Livewire/TwoFactorAuthenticationForm.php
@@ -3,6 +3,8 @@
 namespace Laravel\Jetstream\Http\Livewire;
 
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 use Laravel\Fortify\Actions\DisableTwoFactorAuthentication;
 use Laravel\Fortify\Actions\EnableTwoFactorAuthentication;
 use Laravel\Fortify\Actions\GenerateNewRecoveryCodes;
@@ -10,6 +12,34 @@ use Livewire\Component;
 
 class TwoFactorAuthenticationForm extends Component
 {
+    /**
+     * Indicates if enable is being confirmed.
+     *
+     * @var bool
+     */
+    public $confirmingEnableTwoFactorAuthentication = false;
+
+    /**
+     * Indicates if disable is being confirmed.
+     *
+     * @var bool
+     */
+    public $confirmingDisableTwoFactorAuthentication = false;
+
+    /**
+     * Indicates if regenerate is being confirmed.
+     *
+     * @var bool
+     */
+    public $confirmingRegenerateRecoveryCodes = false;
+
+    /**
+     * The user's current password.
+     *
+     * @var string
+     */
+    public $password = '';
+
     /**
      * Indicates if two factor authentication QR code is being displayed.
      *
@@ -25,6 +55,48 @@ class TwoFactorAuthenticationForm extends Component
     public $showingRecoveryCodes = false;
 
     /**
+     * Confirm that the user would like to enable two factor authentication.
+     *
+     * @return void
+     */
+    public function confirmEnable()
+    {
+        $this->password = '';
+
+        $this->dispatchBrowserEvent('confirming-enable-two-factor-authentication');
+
+        $this->confirmingEnableTwoFactorAuthentication = true;
+    }
+
+    /**
+     * Confirm that the user would like to enable two factor authentication.
+     *
+     * @return void
+     */
+    public function confirmDisable()
+    {
+        $this->password = '';
+
+        $this->dispatchBrowserEvent('confirming-regenerate-recovery-codes');
+
+        $this->confirmingDisableTwoFactorAuthentication = true;
+    }
+
+    /**
+     * Confirm that the user would like to enable two factor authentication.
+     *
+     * @return void
+     */
+    public function confirmRegenerate()
+    {
+        $this->password = '';
+
+        $this->dispatchBrowserEvent('confirming-disable-two-factor-authentication');
+
+        $this->confirmingRegenerateRecoveryCodes = true;
+    }
+
+    /**
      * Enable two factor authentication for the user.
      *
      * @param  \Laravel\Fortify\Actions\EnableTwoFactorAuthentication  $enable
@@ -32,10 +104,20 @@ class TwoFactorAuthenticationForm extends Component
      */
     public function enableTwoFactorAuthentication(EnableTwoFactorAuthentication $enable)
     {
+        $this->resetErrorBag();
+
+        if (! Hash::check($this->password, Auth::user()->password)) {
+            throw ValidationException::withMessages([
+                'password' => [__('This password does not match our records.')],
+            ]);
+        }
+
         $enable(Auth::user());
 
         $this->showingQrCode = true;
         $this->showingRecoveryCodes = true;
+
+        $this->confirmingEnableTwoFactorAuthentication = false;
     }
 
     /**
@@ -46,9 +128,20 @@ class TwoFactorAuthenticationForm extends Component
      */
     public function regenerateRecoveryCodes(GenerateNewRecoveryCodes $generate)
     {
+
+        $this->resetErrorBag();
+
+        if (! Hash::check($this->password, Auth::user()->password)) {
+            throw ValidationException::withMessages([
+                'password' => [__('This password does not match our records.')],
+            ]);
+        }
+
         $generate(Auth::user());
 
         $this->showingRecoveryCodes = true;
+
+        $this->confirmingRegenerateRecoveryCodes = false;
     }
 
     /**
@@ -59,7 +152,18 @@ class TwoFactorAuthenticationForm extends Component
      */
     public function disableTwoFactorAuthentication(DisableTwoFactorAuthentication $disable)
     {
+        $this->resetErrorBag();
+
+        if (! Hash::check($this->password, Auth::user()->password)) {
+            throw ValidationException::withMessages([
+                'password' => [__('This password does not match our records.')],
+            ]);
+        }
+
         $disable(Auth::user());
+
+        $this->confirmingDisableTwoFactorAuthentication = false;
+
     }
 
     /**
--- stubs/livewire/resources/views/profile/two-factor-authentication-form.blade.php
+++ stubs/livewire/resources/views/profile/two-factor-authentication-form.blade.php
@@ -52,12 +52,12 @@
 
         <div class="mt-5">
             @if (! $this->enabled)
-                <x-jet-button type="button" wire:click="enableTwoFactorAuthentication" wire:loading.class="opacity-25" wire:loading.attr="disabled">
+                <x-jet-button type="button" wire:click="confirmEnable" wire:loading.class="opacity-25" wire:loading.attr="disabled">
                     Enable
                 </x-jet-button>
             @else
                 @if ($showingRecoveryCodes)
-                    <x-jet-secondary-button class="mr-3" wire:click="regenerateRecoveryCodes">
+                    <x-jet-secondary-button class="mr-3" wire:click="confirmRegenerate">
                         Regenerate Recovery Codes
                     </x-jet-secondary-button>
                 @else
@@ -66,10 +66,98 @@
                     </x-jet-secondary-button>
                 @endif
 
-                <x-jet-danger-button wire:click="disableTwoFactorAuthentication" wire:loading.class="opacity-25" wire:loading.attr="disabled">
+                <x-jet-danger-button wire:click="confirmDisable" wire:loading.class="opacity-25" wire:loading.attr="disabled">
                     Disable
                 </x-jet-danger-button>
             @endif
         </div>
+
+        <!-- Enable two factor authentication Confirmation Modal -->
+        <x-jet-dialog-modal show="confirmingEnableTwoFactorAuthentication">
+            <x-slot name="title">
+                Enable Two Factor Authentication
+            </x-slot>
+
+            <x-slot name="content">
+                Please enter your password to confirm you would like to enable Two Factor Authentication.
+
+                <div class="mt-4" x-data="{}" x-on:confirming-enable-two-factor-authentication.window="setTimeout(() => $refs.password.focus(), 250)">
+                    <x-jet-input type="password" class="mt-1 block w-3/4" placeholder="Password"
+                                 x-ref="password"
+                                 wire:model="password" />
+
+                    <x-jet-input-error for="password" class="mt-2" />
+                </div>
+            </x-slot>
+
+            <x-slot name="footer">
+                <x-jet-secondary-button wire:click="$toggle('confirmingEnableTwoFactorAuthentication')" wire:loading.class="opacity-25" wire:loading.attr="disabled">
+                    Nevermind
+                </x-jet-secondary-button>
+
+                <x-jet-button class="ml-2" wire:click="enableTwoFactorAuthentication" wire:loading.class="opacity-25" wire:loading.attr="disabled">
+                    Enable Two Factor Authentication
+                </x-jet-button>
+            </x-slot>
+        </x-jet-dialog-modal>
+
+        <!-- Disable Two Factor Authentication Confirmation Modal -->
+        <x-jet-dialog-modal show="confirmingDisableTwoFactorAuthentication">
+            <x-slot name="title">
+                Disable Two Factor Authentication
+            </x-slot>
+
+            <x-slot name="content">
+                Please enter your password to confirm you would like to disable Two Factor Authentication.
+
+                <div class="mt-4" x-data="{}" x-on:confirming-disable-two-factor-authentication.window="setTimeout(() => $refs.password.focus(), 250)">
+                    <x-jet-input type="password" class="mt-1 block w-3/4" placeholder="Password"
+                                 x-ref="password"
+                                 wire:model="password" />
+
+                    <x-jet-input-error for="password" class="mt-2" />
+                </div>
+            </x-slot>
+
+            <x-slot name="footer">
+                <x-jet-secondary-button wire:click="$toggle('confirmingDisableTwoFactorAuthentication')" wire:loading.class="opacity-25" wire:loading.attr="disabled">
+                    Nevermind
+                </x-jet-secondary-button>
+
+                <x-jet-button class="ml-2" wire:click="disableTwoFactorAuthentication" wire:loading.class="opacity-25" wire:loading.attr="disabled">
+                    Disable Two Factor Authentication
+                </x-jet-button>
+            </x-slot>
+        </x-jet-dialog-modal>
+
+        <!-- Regenerate Recovery Codes Confirmation Modal -->
+        <x-jet-dialog-modal show="confirmingRegenerateRecoveryCodes">
+            <x-slot name="title">
+                Regenerate Recovery Codes
+            </x-slot>
+
+            <x-slot name="content">
+                Please enter your password to confirm you would like to regenerate the recovery codes.
+
+                <div class="mt-4" x-data="{}" x-on:confirming-regenerate-recovery-codes.window="setTimeout(() => $refs.password.focus(), 250)">
+                    <x-jet-input type="password" class="mt-1 block w-3/4" placeholder="Password"
+                                 x-ref="password"
+                                 wire:model="password" />
+
+                    <x-jet-input-error for="password" class="mt-2" />
+                </div>
+            </x-slot>
+
+            <x-slot name="footer">
+                <x-jet-secondary-button wire:click="$toggle('confirmingRegenerateRecoveryCodes')" wire:loading.class="opacity-25" wire:loading.attr="disabled">
+                    Nevermind
+                </x-jet-secondary-button>
+
+                <x-jet-button class="ml-2" wire:click="regenerateRecoveryCodes" wire:loading.class="opacity-25" wire:loading.attr="disabled">
+                    Regenerate Recovery Codes
+                </x-jet-button>
+            </x-slot>
+        </x-jet-dialog-modal>
+
     </x-slot>
 </x-jet-action-section>
