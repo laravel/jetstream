fb9d6e255ee3d2c5c1e4831b85c35b053ead51e7
Samy <mail@samynitsche.de>
2020-09-02 15:20:10 +0200

    Replace the "home" route by the "dashboard" route (#16)
    * Replace the "home" route by the "dashboard" route
    
    * Check if the welcome.blade.php file exists
    
    * Update InstallCommand.php
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -43,6 +43,11 @@ class InstallCommand extends Command
         // "Home" Route...
         $this->replaceInFile('/home', '/dashboard', app_path('Providers/RouteServiceProvider.php'));
 
+        if (file_exists(resource_path('views/welcome.blade.php'))) {
+            $this->replaceInFile('/home', '/dashboard', resource_path('views/welcome.blade.php'));
+            $this->replaceInFile('Home', 'Dashboard', resource_path('views/welcome.blade.php'));
+        }
+
         // Fortify Provider...
         $this->installFortifyServiceProvider();
 
