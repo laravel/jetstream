b613d5d6ec6431e1495f9fa3d8b2a2e6bb2c36e2
Taylor Otwell <taylorotwell@gmail.com>
2020-09-02 10:32:42 -0500

    update pruge

--- stubs/inertia/tailwind.config.js
+++ stubs/inertia/tailwind.config.js
@@ -1,7 +1,11 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-    purge: ['./storage/framework/views/*.php', './resources/views/**/*.blade.php', './resources/js/**/*.vue'],
+    purge: [
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.vue',
+    ],
 
     theme: {
         extend: {
--- stubs/livewire/tailwind.config.js
+++ stubs/livewire/tailwind.config.js
@@ -1,7 +1,10 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-    purge: ['./storage/framework/views/*.php'],
+    purge: [
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
 
     theme: {
         extend: {
