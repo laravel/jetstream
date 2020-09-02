ce5cdf6ec707c6f7679221ca375262eb45e09492
Brad <919214+Bradez@users.noreply.github.com>
2020-09-02 14:21:44 +0100

    Include autocomplete on all relevant elements. (#13)
    * Include autocomplete on all relevant elements.
    
    * Update stubs/livewire/resources/views/profile/update-profile-information-form.blade.php
    
    Co-authored-by: Enzo Innocenzi <enzo@innocenzi.dev>
    
    * Update stubs/resources/views/auth/register.blade.php
    
    Co-authored-by: Enzo Innocenzi <enzo@innocenzi.dev>
    
    * Update UpdateProfileInformationForm.vue
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>
    Co-authored-by: Enzo Innocenzi <enzo@innocenzi.dev>

--- stubs/livewire/resources/views/profile/update-profile-information-form.blade.php
+++ stubs/livewire/resources/views/profile/update-profile-information-form.blade.php
@@ -49,7 +49,7 @@
         <!-- Name -->
         <div class="col-span-6 sm:col-span-4">
             <x-jet-label for="name" value="Name" />
-            <x-jet-input id="name" type="text" class="mt-1 block w-full" wire:model="state.name" />
+            <x-jet-input id="name" type="text" class="mt-1 block w-full" wire:model="state.name" autocomplete="name" />
             <x-jet-input-error for="name" class="mt-2" />
         </div>
 
