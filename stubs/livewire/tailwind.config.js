040aa084359e2cc2e1f15533e99a24fb35a43394
Taylor Otwell <taylorotwell@users.noreply.github.com>
2020-09-02 15:32:56 +0000

    Apply fixes from StyleCI

--- stubs/livewire/tailwind.config.js
+++ stubs/livewire/tailwind.config.js
@@ -1,10 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-    purge: [
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-    ],
+    purge: ['./storage/framework/views/*.php', './resources/views/**/*.blade.php'],
 
     theme: {
         extend: {
