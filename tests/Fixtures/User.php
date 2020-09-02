0d3145ca7218e49866759acf45fe4606ea3f694c
Taylor Otwell <taylorotwell@gmail.com>
2020-09-02 09:20:12 -0500

    use numeric ids for users

--- tests/Fixtures/User.php
+++ tests/Fixtures/User.php
@@ -12,35 +12,9 @@ class User extends Authenticatable
     use HasApiTokens, HasTeams;
 
     /**
-     * The "type" of the primary key ID.
-     *
-     * @var string
-     */
-    protected $keyType = 'string';
-
-    /**
-     * Indicates if the IDs are auto-incrementing.
-     *
-     * @var bool
-     */
-    public $incrementing = false;
-
-    /**
      * The attributes that aren't mass assignable.
      *
      * @var array
      */
     protected $guarded = [];
-
-    /**
-     * Handle the model "booted" event.
-     *
-     * @return void
-     */
-    public static function booted()
-    {
-        static::creating(function ($model) {
-            $model->id = $model->id ?: (string) Str::orderedUuid();
-        });
-    }
 }
