973172a8e98f01a225ef5848bc66446233eed7bf
Jignesh <17031402+joisarjignesh@users.noreply.github.com>
2020-09-04 21:19:52 +0530

    set fully qualified URL for login,regiter and logout (#49)

--- stubs/livewire/resources/views/layouts/app.blade.php
+++ stubs/livewire/resources/views/layouts/app.blade.php
@@ -97,10 +97,10 @@
                                     @endif
 
                                     <!-- Authentication -->
-                                    <form method="POST" action="/logout">
+                                    <form method="POST" action="{{ route('logout') }}">
                                         @csrf
 
-                                        <x-jet-dropdown-link href="/logout"
+                                        <x-jet-dropdown-link href="{{ route('logout') }}"
                                                             onclick="event.preventDefault();
                                                                      this.closest('form').submit();">
                                             Logout
@@ -156,10 +156,10 @@
                             @endif
 
                             <!-- Authentication -->
-                            <form method="POST" action="/logout">
+                            <form method="POST" action="{{ route('logout') }}">
                                 @csrf
 
-                                <x-jet-responsive-nav-link href="/logout"
+                                <x-jet-responsive-nav-link href="{{ route('logout') }}"
                                                 onclick="event.preventDefault();
                                                          this.closest('form').submit();">
                                     Logout
