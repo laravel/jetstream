de3107aaf520229f324f7399ed9b2667e14afd94
Taylor Otwell <taylorotwell@gmail.com>
2020-09-04 12:51:44 -0500

    use asset helper

--- stubs/inertia/resources/views/app.blade.php
+++ stubs/inertia/resources/views/app.blade.php
@@ -15,7 +15,7 @@
 
         <!-- Scripts -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.26.0/moment.min.js"></script>
-        <script src="{{ mix('/js/app.js') }}" defer></script>
+        <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
     <body class="font-sans antialiased">
         @inertia
