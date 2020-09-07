dcf391ae2215a3988b458dd155bbf1329c9b702c
pedroufv <pedro.e@ufv.br>
2020-09-06 23:42:13 -0300

    refactor: make auth views translatables (#81)

--- stubs/resources/views/auth/login.blade.php
+++ stubs/resources/views/auth/login.blade.php
@@ -28,19 +28,19 @@
             <div class="block mt-4">
                 <label class="flex items-center">
                     <input type="checkbox" class="form-checkbox">
-                    <span class="ml-2 text-sm text-gray-600">Remember me</span>
+                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
                 </label>
             </div>
 
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        Forgot your password?
+                        {{ __('Forgot your password?') }}
                     </a>
                 @endif
 
                 <x-jet-button class="ml-4">
-                    Login
+                    {{ __('Login') }}
                 </x-jet-button>
             </div>
         </form>
