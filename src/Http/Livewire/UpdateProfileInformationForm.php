5a017c2c9b10c503ea02343dab3ab0feaaef5d19
Dante <35383529+srdante@users.noreply.github.com>
2020-09-08 18:51:31 -0300

    Update UpdateProfileInformationForm.php

--- src/Http/Livewire/UpdateProfileInformationForm.php
+++ src/Http/Livewire/UpdateProfileInformationForm.php
@@ -58,6 +58,20 @@ class UpdateProfileInformationForm extends Component
 
         $this->emit('saved');
     }
+    
+    /**
+     * Delete user's profile photo.
+     *
+     * @param \Laravel\Fortify\Contracts\UpdatesUserProfileInformation $updater
+     *
+     * @return void
+     */
+    public function deleteProfilePhoto(UpdatesUserProfileInformation $updater)
+    {
+        Auth::user()->deleteProfilePhoto();
+
+        return redirect()->route('profile.show');
+    }
 
     /**
      * Get the current user of the application.
