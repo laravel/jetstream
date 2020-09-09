70fb427bf6e1a0ce1f2f2d86766524b63fad7e80
Rama Adi Nugraha <ramatabanan@gmail.com>
2020-09-10 06:30:58 +0800

    add object-cover to photo

--- stubs/livewire/resources/views/profile/update-profile-information-form.blade.php
+++ stubs/livewire/resources/views/profile/update-profile-information-form.blade.php
@@ -28,7 +28,7 @@
 
                 <!-- Current Profile Photo -->
                 <div class="mt-2" x-show="! photoPreview">
-                    <img src="{{ $this->user->profile_photo_url }}" alt="{{ $this->user->name }}" class="rounded-full h-20 w-20">
+                    <img src="{{ $this->user->profile_photo_url }}" alt="{{ $this->user->name }}" class="rounded-full h-20 w-20 object-cover">
                 </div>
 
                 <!-- New Profile Photo Preview -->
