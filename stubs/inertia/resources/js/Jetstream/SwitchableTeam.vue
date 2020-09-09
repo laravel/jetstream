89d7f8b174cd2d4eac4f063492f9db64fc935554
Taylor Otwell <taylorotwell@gmail.com>
2020-09-09 10:53:50 -0500

    update vue component

--- stubs/inertia/resources/js/Jetstream/SwitchableTeam.vue
+++ stubs/inertia/resources/js/Jetstream/SwitchableTeam.vue
@@ -7,7 +7,7 @@
                 <svg class="mr-2 h-5 w-5 text-green-400" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
             @endif
 
-            <div>
+            <div class="truncate">
                 {{ $team->name }}
             </div>
         </div>
