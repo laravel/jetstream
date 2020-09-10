651a1fc592710e6de04df298ff8f5ab21a9b34d8
Jignesh Joisar <jigneshjoisar@gmail.com>
2020-09-10 23:59:13 +0530

    link to route reset password

--- stubs/resources/views/auth/forgot-password.blade.php
+++ stubs/resources/views/auth/forgot-password.blade.php
@@ -16,7 +16,7 @@
 
         <x-jet-validation-errors class="mb-4" />
 
-        <form method="POST" action="/forgot-password">
+        <form method="POST" action="{{ route('password.request') }}">
             @csrf
 
             <div class="block">
