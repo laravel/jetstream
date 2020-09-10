192e5d392af0e0a07d68c2ba0958a6c13f91f976
Taylor Otwell <taylorotwell@users.noreply.github.com>
2020-09-10 13:16:07 +0000

    Apply fixes from StyleCI

--- src/Http/Controllers/Inertia/UserProfileController.php
+++ src/Http/Controllers/Inertia/UserProfileController.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
-use Inertia\Inertia;
 use Jenssegers\Agent\Agent;
 use Laravel\Jetstream\Jetstream;
 
