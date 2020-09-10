e88db7c7fbd67be02a9091e8378c6e4d26ac84bd
Rama Adi Nugraha <ramatabanan@gmail.com>
2020-09-10 10:34:29 +0800

    Added object-cover to profile images (#122)
    * Added object-cover to avatar on AppLayout.vue
    
    * Add object-cover to app.blade.php
    
    * add object-cover to photo
    
    * added object-cover

--- stubs/livewire/resources/views/layouts/app.blade.php
+++ stubs/livewire/resources/views/layouts/app.blade.php
@@ -45,7 +45,7 @@
                             <x-jet-dropdown align="right" width="48">
                                 <x-slot name="trigger">
                                     <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out">
-                                        <img class="h-8 w-8 rounded-full" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+                                        <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
                                     </button>
                                 </x-slot>
 
