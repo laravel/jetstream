20f2d25e39d16cc49f3042d0e6d557d2c7153e3e
Taylor Otwell <taylorotwell@users.noreply.github.com>
2020-09-10 18:16:48 +0000

    Apply fixes from StyleCI

--- src/Http/Controllers/Inertia/ApiTokenController.php
+++ src/Http/Controllers/Inertia/ApiTokenController.php
@@ -5,7 +5,6 @@ namespace Laravel\Jetstream\Http\Controllers\Inertia;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Validator;
-use Inertia\Inertia;
 use Laravel\Jetstream\Jetstream;
 
 class ApiTokenController extends Controller
