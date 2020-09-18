321596b8d084ca27a51d3a2e966b2b1ddc3000a0
Luis Parrado <luisprmat@gmail.com>
2020-09-18 07:26:53 -0500

    [1.x] Add translation support  to confirms-password.blade.php component (#221)
    * Add translate __(' ') notation to some jetstream components
    
    * Update confirms-password.blade.php to include translation suppot

--- resources/views/components/confirms-password.blade.php
+++ resources/views/components/confirms-password.blade.php
@@ -24,7 +24,7 @@
         {{ $content }}
 
         <div class="mt-4" x-data="{}" x-on:confirming-password.window="setTimeout(() => $refs.confirmable_password.focus(), 250)">
-            <x-jet-input type="password" class="mt-1 block w-3/4" placeholder="Password"
+            <x-jet-input type="password" class="mt-1 block w-3/4" placeholder="{{ __('Password') }}"
                         x-ref="confirmable_password"
                         wire:model.defer="confirmablePassword"
                         wire:keydown.enter="confirmPassword" />
@@ -35,7 +35,7 @@
 
     <x-slot name="footer">
         <x-jet-secondary-button wire:click="stopConfirmingPassword" wire:loading.attr="disabled">
-            Nevermind
+            {{ __('Nevermind') }}
         </x-jet-secondary-button>
 
         <x-jet-button class="ml-2" wire:click="confirmPassword" wire:loading.attr="disabled">
