c8e5db0f6833a610015bdddc42097543d67fbc08
Björn Nilsved <nilsved@gmail.com>
2020-09-21 15:12:31 +0200

    Explicitly set table name on pivot model
    Makes it easier to treat Jetstream default and customized Membership pivot models the same way.
    
    As the table name is hard-coded in the published migration, it may be as well here.

--- src/Membership.php
+++ src/Membership.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Relations\Pivot;
 
 abstract class Membership extends Pivot
 {
-    //
+    /**
+     * The table associated with the pivot model.
+     *
+     * @var string
+     */
+    protected $table = 'team_user';
 }
