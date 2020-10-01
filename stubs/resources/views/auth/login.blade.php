59280c59eb539698dabb36eb8a77cd27bcd0417b
Chrissi2812 <cf@pixel-kraft.de>
2020-10-01 15:19:36 +0200

    Use revealable-password component in login form

--- stubs/resources/views/auth/login.blade.php
+++ stubs/resources/views/auth/login.blade.php
@@ -22,7 +22,7 @@
 
             <div class="mt-4">
                 <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+                <x-jet-revealable-password id="password" class="block mt-1 w-full" name="password" required autocomplete="current-password" />
             </div>
 
             <div class="block mt-4">
