569b33bc93d63b555c628b1324abef295e48171f
drjdr <joris@deranter.eu>
2020-10-07 18:10:23 +0200

    Update profile is easier if the full profile is given

--- stubs/inertia/resources/js/Pages/Profile/Show.vue
+++ stubs/inertia/resources/js/Pages/Profile/Show.vue
@@ -9,8 +9,7 @@
         <div>
             <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
                 <update-profile-information-form
-                            :name="$page.user.name"
-                            :email="$page.user.email" />
+                    :user="$page.user" />
 
                 <jet-section-border />
 
