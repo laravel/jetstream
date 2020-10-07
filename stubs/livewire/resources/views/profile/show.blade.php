081334ae7c2061d691879210b26c0d2d92e86f1c
Stefan Gasser <stefan.gasser@additive.eu>
2020-10-07 16:22:02 +0200

    Add better if to show/hide Fortify features in profile #321

--- stubs/livewire/resources/views/profile/show.blade.php
+++ stubs/livewire/resources/views/profile/show.blade.php
@@ -7,25 +7,27 @@
 
     <div>
         <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
-            @livewire('profile.update-profile-information-form')
+            @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::updateProfileInformation()))
+                @livewire('profile.update-profile-information-form')
+
+                <x-jet-section-border />
+            @endif
 
             @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::updatePasswords()))
-                <x-jet-section-border />
-            
                 <div class="mt-10 sm:mt-0">
                     @livewire('profile.update-password-form')
                 </div>
+
+                <x-jet-section-border />
             @endif
 
             @if (Laravel\Fortify\Features::canManageTwoFactorAuthentication())
-                <x-jet-section-border />
-
                 <div class="mt-10 sm:mt-0">
                     @livewire('profile.two-factor-authentication-form')
                 </div>
-            @endif
 
-            <x-jet-section-border />
+                <x-jet-section-border />
+            @endif
 
             <div class="mt-10 sm:mt-0">
                 @livewire('profile.logout-other-browser-sessions-form')
