e378829b1d51255c447adc76a3527ba68786ca3d
Claudio Dekker <claudiodekker@users.noreply.github.com>
2020-10-08 16:44:45 +0200

    Workaround for Vapor & non-mix environments

--- src/Http/Middleware/ShareInertiaData.php
+++ src/Http/Middleware/ShareInertiaData.php
@@ -21,7 +21,20 @@ class ShareInertiaData
     public function handle($request, $next)
     {
         Inertia::version(function () {
-            return md5_file(public_path('mix-manifest.json'));
+            // When we are running on Laravel Vapor, asset URLs are automatically generated
+            // for us whenever an asset gets deployed. Because of this, there is no need
+            // to retrieve the whole file for hashing, as we can use the URL instead.
+            if (config('app.asset_url')) {
+                return md5(config('app.asset_url'));
+            }
+
+            // Alternatively, when we are running in a regular (non-serverless) environment
+            // we'll attempt to use the Laravel Mix asset manifest to generate our hash.
+            if (file_exists($manifest = public_path('js/mix-manifest.json'))) {
+                return md5_file($manifest);
+            }
+
+            return null;
         });
 
         Inertia::share(array_filter([
