8baf26f99216fb3980bb179383829bb278f5b7fd
Joel Butcher <joel@joelbutcher.co.uk>
2020-10-09 20:10:20 +0100

    Added social provider events

---
+++ src/Events/SocialProviderCreated.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Laravel\Jetstream\Events;
+
+class SocialProviderCreated extends TeamEvent
+{
+    //
+}
---
+++ src/Events/SocialProviderEvent.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Laravel\Jetstream\Events;
+
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+abstract class SocialProviderEvent
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * The social provider instance.
+     *
+     * @var \App\SocialProvider
+     */
+    public $provider;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param  \App\SocialProvider  $provider
+     * @return void
+     */
+    public function __construct($provider)
+    {
+        $this->provider = $provider;
+    }
+}
