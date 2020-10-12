5787e3e98bb53401925417aacf6de8f83cbb8e7a
Joel Butcher <joel@joelbutcher.co.uk>
2020-10-12 21:14:03 +0100

    Update HasSocialProviders trait methods

--- src/HasSocialProviders.php
+++ src/HasSocialProviders.php
@@ -2,21 +2,28 @@
 
 namespace Laravel\Jetstream;
 
+use Illuminate\Support\Str;
+
 trait HasSocialProviders
 {
-    public function allProviders()
+    /**
+     * Determine if the user has a specific provider type.
+     *
+     * @param  string  $providerName
+     * @return bool
+     */
+    public function hasSocialProviderType(string $providerName)
     {
-
+        return $this->socialProviders->contains('provider_name', Str::lower($providerName));
     }
 
     /**
      * Get all of the social providers belonging to the user.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
      */
     public function socialProviders()
     {
-        return $this->belongsToMany(Jetstream::teamModel(), Jetstream::membershipModel())
-            ->withPivot('role')
-            ->withTimestamps()
-            ->as('membership');
+        return $this->hasMany(Jetstream::socialProviderModel(), 'user_id');
     }
 }
