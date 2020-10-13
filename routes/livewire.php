92bfd3eb6f844bfa46755cdad6137cd75b59b375
= <joel@joelbutcher.co.uk>
2020-10-13 15:24:28 +0100

    Added Livewire routes for Socialite OAuth control

--- routes/livewire.php
+++ routes/livewire.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Laravel\Jetstream\Http\Controllers\CurrentTeamController;
 use Laravel\Jetstream\Http\Controllers\Livewire\ApiTokenController;
+use Laravel\Jetstream\Http\Controllers\Livewire\ConnectedAccountsController;
 use Laravel\Jetstream\Http\Controllers\Livewire\TeamController;
 use Laravel\Jetstream\Http\Controllers\Livewire\UserProfileController;
 use Laravel\Jetstream\Http\Controllers\TeamInvitationController;
@@ -28,6 +29,12 @@ Route::group(['middleware' => config('jetstream.middleware', ['web'])], function
             Route::get('/team-invitations/{invitation}', [TeamInvitationController::class, 'accept'])
                         ->middleware(['signed'])
                         ->name('team-invitations.accept');
+        }
+
+        // Socialite...
+        if (Jetstream::hasSocialiteFeatures()) {
+            Route::get('/connected-accounts/{provider}', [ConnectedAccountsController::class, 'show']); // Redirect to provider...
+            Route::post('/connected-accounts/{provider}', [ConnectedAccountsController::class, 'store']); // Callback URL for provider...
         }
     });
 });
