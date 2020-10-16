fb2a09a979da22cbdb461e6243442f6c169c4376
Aaron Francis <aarondfrancis@gmail.com>
2020-10-16 16:59:50 -0500

    Use the form-select class

--- stubs/inertia/resources/js/Jetstream/Select.vue
+++ stubs/inertia/resources/js/Jetstream/Select.vue
@@ -1,18 +1,12 @@
 <template>
-    <div class='relative'>
-        <select class="form-input w-full rounded-md shadow-sm" v-bind='$attrs' :value='value' @input="$emit('input', $event.target.value)" ref="input">
-            <option v-if='placeholder' :value='null' disabled>{{ placeholder }}</option>
-            <slot></slot>
-        </select>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class='absolute fill-current text-gray-300 right-0 -mt-2 mr-2 h-4 w-4 pointer-events-none' style='top: 50%'>
-            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-        </svg>
-    </div>
+    <select class="form-select w-full rounded-md shadow-sm" :value='value' @input="$emit('input', $event.target.value)" ref="select">
+        <option v-if='placeholder' :value='null' disabled>{{ placeholder }}</option>
+        <slot></slot>
+    </select>
 </template>
 
 <script>
     export default {
-        inheritAttrs: false,
         props: {
             value: {
                 required: false,
@@ -25,7 +19,7 @@
 
         methods: {
             focus() {
-                this.$refs.input.focus()
+                this.$refs.select.focus()
             }
         }
     }
