b322213a522029276eeb15826e10a1d52605097e
Taylor Otwell <taylorotwell@gmail.com>
2020-10-19 14:34:24 -0500

    working on livewire support

--- stubs/livewire/resources/views/teams/team-member-manager.blade.php
+++ stubs/livewire/resources/views/teams/team-member-manager.blade.php
@@ -74,6 +74,43 @@
         </div>
     @endif
 
+    @if ($team->teamInvitations->isNotEmpty() && Gate::check('addTeamMember', $team))
+        <x-jet-section-border />
+
+        <!-- Team Member Invitations -->
+        <div class="mt-10 sm:mt-0">
+            <x-jet-action-section>
+                <x-slot name="title">
+                    {{ __('Pending Team Invitations') }}
+                </x-slot>
+
+                <x-slot name="description">
+                    {{ __('These people have been invited to your team and have been sent an invitation email. They may join the team by accepting the email invitation.') }}
+                </x-slot>
+
+                <x-slot name="content">
+                    <div class="space-y-6">
+                        @foreach ($team->teamInvitations as $invitation)
+                            <div class="flex items-center justify-between">
+                                <div class="text-gray-600">{{ $invitation->email }}</div>
+
+                                <div class="flex items-center">
+                                    @if (Gate::check('removeTeamMember', $team))
+                                        <!-- Cancel Team Invitation -->
+                                        <button class="cursor-pointer ml-6 text-sm text-red-500 focus:outline-none"
+                                                            wire:click="cancelTeamInvitation({{ $invitation->id }})">
+                                            Cancel
+                                        </button>
+                                    @endif
+                                </div>
+                            </div>
+                        @endforeach
+                    </div>
+                </x-slot>
+            </x-jet-action-section>
+        </div>
+    @endif
+
     @if ($team->users->isNotEmpty())
         <x-jet-section-border />
 
