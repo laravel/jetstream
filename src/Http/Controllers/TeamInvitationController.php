87900d5ea2f8cd3b6bf9643335cd11601d5ee37c
Taylor Otwell <taylorotwell@users.noreply.github.com>
2020-10-20 19:22:30 +0000

    Apply fixes from StyleCI

--- src/Http/Controllers/TeamInvitationController.php
+++ src/Http/Controllers/TeamInvitationController.php
@@ -30,7 +30,7 @@ class TeamInvitationController extends Controller
         $invitation->delete();
 
         return redirect(config('fortify.home'))->with('flash', [
-            'banner' => __('Great! You have accepted the invitation to join the :team team.', ['team' => $invitation->team->name])
+            'banner' => __('Great! You have accepted the invitation to join the :team team.', ['team' => $invitation->team->name]),
         ]);
     }
 
