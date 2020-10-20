e26a84edf6ae6cbd4f888fae44e70ad0ee287e02
Taylor Otwell <taylorotwell@gmail.com>
2020-10-20 17:21:33 -0500

    updating formatting

--- stubs/inertia/resources/js/Jetstream/Banner.vue
+++ stubs/inertia/resources/js/Jetstream/Banner.vue
@@ -1,12 +1,16 @@
 <template>
     <div>
-        <div class="bg-indigo-500" v-if="show && message">
+        <div :class="{ 'bg-indigo-500': style == 'success', 'bg-red-700': style == 'danger' }" v-if="show && message">
             <div class="max-w-screen-xl mx-auto py-2 px-3 sm:px-6 lg:px-8">
                 <div class="flex items-center justify-between flex-wrap">
                     <div class="w-0 flex-1 flex items-center">
-                        <span class="flex p-2 rounded-lg bg-indigo-600">
-                            <svg class="h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <span class="flex p-2 rounded-lg" :class="{ 'bg-indigo-600': style == 'success', 'bg-red-600': style == 'danger' }">
+                            <svg class="h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" v-if="style == 'success'">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                            </svg>
+
+                            <svg class="h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" v-if="style == 'danger'">
+                              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
                             </svg>
                         </span>
 
@@ -24,7 +28,8 @@
                     <div class="flex-shrink-0 sm:ml-3">
                         <button
                         type="button"
-                        class="-mr-1 flex p-2 rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600 sm:-mr-2 transition ease-in-out duration-150"
+                        class="-mr-1 flex p-2 rounded-md focus:outline-none sm:-mr-2 transition ease-in-out duration-150"
+                        :class="{ 'hover:bg-indigo-600 focus:bg-indigo-600': style == 'success', 'hover:bg-red-600 focus:bg-red-600': style == 'danger' }"
                         aria-label="Dismiss"
                         @click.prevent="show = false">
                             <svg class="h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -42,6 +47,7 @@
     export default {
         data() {
             return {
+                style: this.$page.props.jetstream.flash?.bannerStyle || 'success',
                 show: true,
                 message: this.$page.props.jetstream.flash?.banner || '',
             }
