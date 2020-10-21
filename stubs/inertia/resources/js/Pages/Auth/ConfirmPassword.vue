1b5816ab1b6a6f8f01fe7930308c02a24ea9f005
Taylor Otwell <taylor@laravel.com>
2020-10-21 15:31:04 -0500

    Confirm Password (#374)
    * support the confirm password view
    
    * add blade confirm password screen

---
+++ stubs/inertia/resources/js/Pages/Auth/ConfirmPassword.vue
@@ -0,0 +1,60 @@
+<template>
+    <jet-authentication-card>
+        <template #logo>
+            <jet-authentication-card-logo />
+        </template>
+
+        <div class="mb-4 text-sm text-gray-600">
+            This is a secure area of the application. Please confirm your password before continuing.
+        </div>
+
+        <jet-validation-errors class="mb-4" />
+
+        <form @submit.prevent="submit">
+            <div>
+                <jet-label for="password" value="Password" />
+                <jet-input id="password" type="password" class="mt-1 block w-full" v-model="form.password" required autocomplete="current-password" autofocus />
+            </div>
+
+            <div class="flex justify-end mt-4">
+                <jet-button class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                    Confirm
+                </jet-button>
+            </div>
+        </form>
+    </jet-authentication-card>
+</template>
+
+<script>
+    import JetAuthenticationCard from '@/Jetstream/AuthenticationCard'
+    import JetAuthenticationCardLogo from '@/Jetstream/AuthenticationCardLogo'
+    import JetButton from '@/Jetstream/Button'
+    import JetInput from '@/Jetstream/Input'
+    import JetLabel from '@/Jetstream/Label'
+    import JetValidationErrors from '@/Jetstream/ValidationErrors'
+
+    export default {
+        components: {
+            JetAuthenticationCard,
+            JetAuthenticationCardLogo,
+            JetButton,
+            JetInput,
+            JetLabel,
+            JetValidationErrors
+        },
+
+        data() {
+            return {
+                form: this.$inertia.form({
+                    password: '',
+                })
+            }
+        },
+
+        methods: {
+            submit() {
+                this.form.post(this.route('password.confirm'))
+            }
+        }
+    }
+</script>
---
+++ stubs/resources/views/auth/confirm-password.blade.php
@@ -0,0 +1,28 @@
+<x-guest-layout>
+    <x-jet-authentication-card>
+        <x-slot name="logo">
+            <x-jet-authentication-card-logo />
+        </x-slot>
+
+        <div class="mb-4 text-sm text-gray-600">
+            This is a secure area of the application. Please confirm your password before continuing.
+        </div>
+
+        <x-jet-validation-errors class="mb-4" />
+
+        <form method="POST" action="{{ route('password.confirm') }}">
+            @csrf
+
+            <div>
+                <x-jet-label for="password" value="{{ __('Password') }}" />
+                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" autofocus />
+            </div>
+
+            <div class="flex justify-end mt-4">
+                <x-jet-button class="ml-4">
+                    {{ __('Confirm') }}
+                </x-jet-button>
+            </div>
+        </form>
+    </x-jet-authentication-card>
+</x-guest-layout>
