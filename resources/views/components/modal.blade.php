e4c7b3539d1143f76d4a081dede487de0c1cc49a
Andrew P <andreich1980@gmail.com>
2020-10-22 12:41:11 +0300

    Make Modal scrollable
    * inset-0, overflow-y-auto - scroll the modal
    * py-6 - add bottom padding for the modal parent

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -54,7 +54,7 @@ switch ($maxWidth ?? '2xl') {
     x-on:keydown.shift.tab.prevent="prevFocusable().focus()"
     x-show="show"
     id="{{ $id }}"
-    class="fixed top-0 inset-x-0 px-4 pt-6 z-50 sm:px-0"
+    class="fixed inset-0 overflow-y-auto px-4 py-6 z-50 sm:px-0"
     style="display: none;"
 >
     <div x-show="show" class="fixed inset-0 transform transition-all" x-on:click="show = false" x-transition:enter="ease-out duration-300"
