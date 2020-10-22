1963d389e244f83609227f828739f83e07c8e074
Andrew P <andreich1980@gmail.com>
2020-10-22 15:50:10 +0300

    Inertia: Fix for FF

--- stubs/inertia/resources/js/Jetstream/Modal.vue
+++ stubs/inertia/resources/js/Jetstream/Modal.vue
@@ -19,7 +19,7 @@
                         leave-active-class="ease-in duration-200"
                         leave-class="opacity-100 translate-y-0 sm:scale-100"
                         leave-to-class="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
-                    <div v-show="show" class="bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:w-full sm:mx-auto" :class="maxWidthClass">
+                    <div v-show="show" class="mb-6 bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:w-full sm:mx-auto" :class="maxWidthClass">
                         <slot></slot>
                     </div>
                 </transition>
