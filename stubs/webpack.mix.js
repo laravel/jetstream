b2dd032517d2d34c562257bb42741055d7af39d7
Devin Gray <11573528+devingray@users.noreply.github.com>
2020-10-22 14:36:36 +0700

    Fix typo

--- stubs/webpack.mix.js
+++ stubs/webpack.mix.js
@@ -1,5 +1,5 @@
 const mix = require('laravel-mix');
-const config = require('.webpack.config');
+const config = require('./webpack.config');
 
 /*
  |--------------------------------------------------------------------------
