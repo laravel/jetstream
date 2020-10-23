254c3daca3cfd4792a3b2a5dfcf2461fbe484b09
Marek Szymczuk <marek@osiemsiedem.com>
2020-10-23 23:27:16 +0200

    Fix truncation of long messages

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -12,7 +12,7 @@
             ">
     <div class="max-w-screen-xl mx-auto py-2 px-3 sm:px-6 lg:px-8">
         <div class="flex items-center justify-between flex-wrap">
-            <div class="w-0 flex-1 flex items-center">
+            <div class="w-0 flex-1 flex items-center min-w-0">
                 <span class="flex p-2 rounded-lg" :class="{ 'bg-indigo-600': style == 'success', 'bg-red-600': style == 'danger' }">
                     <svg class="h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
