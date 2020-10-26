c946f5ef1a6a0cae61bb59c0766e0068027d021a
Dries Vints <dries@vints.io>
2020-10-26 11:15:46 +0100

    Update action-message.blade.php

--- resources/views/components/action-message.blade.php
+++ resources/views/components/action-message.blade.php
@@ -5,5 +5,5 @@
     x-show.transition.opacity.out.duration.1500ms="shown"
     style="display: none;"
     {{ $attributes->merge(['class' => 'text-sm text-gray-600']) }}>
-    {{ $slot->isEmpty() ? 'Saved.' : $slot}}
+    {{ $slot->isEmpty() ? 'Saved.' : $slot }}
 </div>
