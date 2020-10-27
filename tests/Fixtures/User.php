ac4a285e276b8e35b9e538c94d00ada1f376417c
lostdesign <info@lost.design>
2020-10-27 22:25:59 +0100

    add HasProfilePhoto trait to fixture

--- tests/Fixtures/User.php
+++ tests/Fixtures/User.php
@@ -5,10 +5,11 @@ namespace Laravel\Jetstream\Tests\Fixtures;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Jetstream\HasTeams;
 use Laravel\Sanctum\HasApiTokens;
+use Laravel\Jetstream\HasProfilePhoto;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasTeams;
+    use HasApiTokens, HasTeams, HasProfilePhoto;
 
     /**
      * The attributes that aren't mass assignable.
