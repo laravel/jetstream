a866f6ab6fe474fa90624d7045e4eade8d06175e
Taylor Maguire <taylor@codingfoundry.com>
2020-10-30 21:55:24 -0700

    Change Add Team Member Roles to Buttons from Div

--- stubs/inertia/resources/js/Pages/Teams/TeamMemberManager.vue
+++ stubs/inertia/resources/js/Pages/Teams/TeamMemberManager.vue
@@ -32,9 +32,9 @@
                         <jet-label for="roles" value="Role" />
                         <jet-input-error :message="addTeamMemberForm.error('role')" class="mt-2" />
 
-                        <div class="mt-1 border border-gray-200 rounded-lg cursor-pointer">
-                            <div class="px-4 py-3"
-                                            :class="{'border-t border-gray-200': i > 0}"
+                        <div class="mt-1 border border-gray-200 rounded-lg cursor-pointer relative z-0">
+                            <button type="button" class="px-4 py-3 relative inline-flex rounded-lg w-full focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue"
+                                            :class="{'border-t border-gray-200 rounded-t-none': i > 0, 'rounded-b-none': i != Object.keys(availableRoles).length - 1}"
                                             @click="addTeamMemberForm.role = role.key"
                                             v-for="(role, i) in availableRoles"
                                             :key="role.key"
@@ -54,7 +54,7 @@
                                         {{ role.description }}
                                     </div>
                                 </div>
-                            </div>
+                            </button>
                         </div>
                     </div>
                 </template>
