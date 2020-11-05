9491a9c5e563ad2842f84ed4fc5bc3aac7e36f69
Zemmouri Rediane <z.rediane24@gmail.com>
2020-11-05 17:29:53 +0100

    Undo styling changes in Inertia.php router

--- routes/inertia.php
+++ routes/inertia.php
@@ -16,17 +16,17 @@ Route::group(['middleware' => config('jetstream.middleware', ['web'])], function
     Route::group(['middleware' => ['auth', 'verified']], function () {
         // User & Profile...
         Route::get('/user/profile', [UserProfileController::class, 'show'])
-            ->name('profile.show');
+                    ->name('profile.show');
 
         Route::delete('/user/other-browser-sessions', [OtherBrowserSessionsController::class, 'destroy'])
-            ->name('other-browser-sessions.destroy');
+                    ->name('other-browser-sessions.destroy');
 
         Route::delete('/user/profile-photo', [ProfilePhotoController::class, 'destroy'])
-            ->name('current-user-photo.destroy');
+                    ->name('current-user-photo.destroy');
 
-        if (Jetstream::hasAccountDeletionFeatures()) {
+        if(Jetstream::hasAccountDeletionFeatures()){
             Route::delete('/user', [CurrentUserController::class, 'destroy'])
-                ->name('current-user.destroy');
+                        ->name('current-user.destroy');
         }
 
         // API...
@@ -50,11 +50,11 @@ Route::group(['middleware' => config('jetstream.middleware', ['web'])], function
             Route::delete('/teams/{team}/members/{user}', [TeamMemberController::class, 'destroy'])->name('team-members.destroy');
 
             Route::get('/team-invitations/{invitation}', [TeamInvitationController::class, 'accept'])
-                ->middleware(['signed'])
-                ->name('team-invitations.accept');
+                        ->middleware(['signed'])
+                        ->name('team-invitations.accept');
 
             Route::delete('/team-invitations/{invitation}', [TeamInvitationController::class, 'destroy'])
-                ->name('team-invitations.destroy');
+                        ->name('team-invitations.destroy');
         }
     });
 });
