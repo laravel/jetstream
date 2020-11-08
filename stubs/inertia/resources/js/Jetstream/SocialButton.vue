b2cecfaaefe42ebf68ddbf76dd8d1d27ae910e1e
Joel Butcher <joel@joelbutcher.co.uk>
2020-11-08 15:22:46 +0000

    Added vue js files for socialite buttons

---
+++ stubs/inertia/resources/js/Jetstream/SocialButton.vue
@@ -0,0 +1,27 @@
+<template>
+    <a :href="href" :style="styles">
+        <slot></slot>
+    </a>
+</template>
+
+<script>
+    export default {
+        props: {
+            href: {
+                type: String,
+            },
+            color: {
+                type: String,
+                default: null,
+            }
+        },
+
+        computed: {
+            styles() {
+                return this.color !== null
+                    ? `color: ${this.color};`
+                    : 'color: black';
+            },
+        }
+    }
+</script>
