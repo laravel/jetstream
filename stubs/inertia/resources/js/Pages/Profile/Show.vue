0697b2cefe21a5bb0fd547e2394bb8d81ce33745
Dries Vints <dries@vints.io>
2020-11-12 15:10:04 +0100

    Fix feature toggles in Inertia

--- stubs/inertia/resources/js/Pages/Profile/Show.vue
+++ stubs/inertia/resources/js/Pages/Profile/Show.vue
@@ -8,19 +8,23 @@
 
         <div>
             <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
-                <update-profile-information-form :user="$page.user" />
+                <div v-if="$page.jetstream.canManageUpdateProfileInformation">
+                    <update-profile-information-form :user="$page.user" />
 
-                <jet-section-border />
-
-                <update-password-form class="mt-10 sm:mt-0" />
-
-                <div v-if="$page.jetstream.canManageTwoFactorAuthentication">
                     <jet-section-border />
-
-                    <two-factor-authentication-form class="mt-10 sm:mt-0" />
                 </div>
 
-                <jet-section-border />
+                <div v-if="$page.jetstream.canManageUpdatePassword">
+                    <update-password-form class="mt-10 sm:mt-0" />
+
+                    <jet-section-border />
+                </div>
+
+                <div v-if="$page.jetstream.canManageTwoFactorAuthentication">
+                    <two-factor-authentication-form class="mt-10 sm:mt-0" />
+
+                    <jet-section-border />
+                </div>
 
                 <logout-other-browser-sessions-form :sessions="sessions" class="mt-10 sm:mt-0" />
 
