162c65fe43fcb9830e859b4fb607e79fb4a53361
Mohamed Said <themohamedsaid@gmail.com>
2020-11-12 18:01:04 +0200

    work on inertia

--- stubs/inertia/resources/js/Pages/Auth/Register.vue
+++ stubs/inertia/resources/js/Pages/Auth/Register.vue
@@ -27,6 +27,16 @@
                 <jet-input id="password_confirmation" type="password" class="mt-1 block w-full" v-model="form.password_confirmation" required autocomplete="new-password" />
             </div>
 
+            <div class="mt-4" v-if="$page.props.jetstream.hasTermsAndPrivacyPolicyFeature">
+                <jet-label for="terms">
+                    <input class="mr-2 leading-tight" type="checkbox" name="terms" id="terms" v-model="form.terms">
+
+                    <span class="mt-1">
+                        I agree to the <a target="_blank" href="/terms" class="underline text-sm text-gray-600 hover:text-gray-900">Terms of Service</a> and <a target="_blank" href="/policy" class="underline text-sm text-gray-600 hover:text-gray-900">Privacy Policy</a>
+                    </span>
+                </jet-label>
+            </div>
+
             <div class="flex items-center justify-end mt-4">
                 <inertia-link :href="route('login')" class="underline text-sm text-gray-600 hover:text-gray-900">
                     Already registered?
@@ -64,7 +74,8 @@
                     name: '',
                     email: '',
                     password: '',
-                    password_confirmation: ''
+                    password_confirmation: '',
+                    terms: null,
                 })
             }
         },
