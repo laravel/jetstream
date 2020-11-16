55e2445073ac8da234e66c8b4cf47602d7a71740
drjdr <joris@deranter.eu>
2020-11-16 23:01:20 +0100

    formatting

--- database/factories/TeamFactory.php
+++ database/factories/TeamFactory.php
@@ -1,8 +1,8 @@
 <?php
 namespace Database\Factories;
 
-use App\Models\User;
 use App\Models\Team;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class TeamFactory extends Factory
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,8 +2,8 @@
 
 namespace Database\Factories;
 
-use App\Models\User;
 use App\Models\Team;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
@@ -15,7 +15,6 @@ class UserFactory extends Factory
      * @var string
      */
     protected $model = User::class;
-
 
     /**
      * Configure the factory.
