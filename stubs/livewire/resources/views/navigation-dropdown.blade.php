788d0314430b5648613914fafb0ee73482b23fd6
Rafael Laurindo <rafaelfilholm@gmail.com>
2020-11-20 10:50:29 -0300

    [2.x] Checking for photos feature in mobile navigation (#478)
    * Update navigation-dropdown.blade.php
    
    * checking for photos feature in nav

--- stubs/livewire/resources/views/navigation-dropdown.blade.php
+++ stubs/livewire/resources/views/navigation-dropdown.blade.php
@@ -145,11 +145,13 @@
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
             <div class="flex items-center px-4">
-                <div class="flex-shrink-0">
-                    <img class="h-10 w-10 rounded-full" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
-                </div>
+                @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
+                    <div class="flex-shrink-0 mr-3">
+                        <img class="h-10 w-10 rounded-full" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+                    </div>
+                @endif
 
-                <div class="ml-3">
+                <div>
                     <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
                     <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
                 </div>
