776b99c680b85bce5dc3edd8e6cf370b176cc8d1
Murtaza Sultani <murtaz1392@gmail.com>
2020-11-24 14:06:13 +0430

    Update input style to tailwindcss v2

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'form-input rounded-md shadow-sm']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'rounded-md shadow-sm border-gray-300']) !!}>
--- stubs/inertia/resources/js/Jetstream/Input.vue
+++ stubs/inertia/resources/js/Jetstream/Input.vue
@@ -1,5 +1,5 @@
 <template>
-    <input class="form-input rounded-md shadow-sm" :value="value" @input="$emit('input', $event.target.value)" ref="input">
+    <input class="rounded-md shadow-sm border-gray-300" :value="value" @input="$emit('input', $event.target.value)" ref="input">
 </template>
 
 <script>
