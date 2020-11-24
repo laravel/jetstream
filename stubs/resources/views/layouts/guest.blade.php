b9b540e39848f22ba69305749a560b135d7f3823
Dries Vints <dries@vints.io>
2020-11-24 15:19:35 +0100

    Use Alpine directly for guest layout (#485)

--- stubs/resources/views/layouts/guest.blade.php
+++ stubs/resources/views/layouts/guest.blade.php
@@ -14,7 +14,7 @@
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
         <!-- Scripts -->
-        <script src="{{ mix('js/app.js') }}" defer></script>
+        <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.7.3/dist/alpine.js" defer></script>
     </head>
     <body>
         <div class="font-sans text-gray-900 antialiased">
