7febde56e685414112c1989dce47edf0a1963cb5
Ray Nirola <54945029+raynirola@users.noreply.github.com>
2020-12-09 19:59:55 +0530

    Fix avatar aspect ration in resposive navigation menu

--- stubs/livewire/resources/views/navigation-dropdown.blade.php
+++ stubs/livewire/resources/views/navigation-dropdown.blade.php
@@ -126,7 +126,7 @@
         <div class="pt-4 pb-1 border-t border-gray-200">
             <div class="flex items-center px-4">
                 <div class="flex-shrink-0">
-                    <img class="h-10 w-10 rounded-full" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+                    <img class="h-10 w-10 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
                 </div>
 
                 <div class="ml-3">
