6cda0cf93b0fadc7b28c578cace1b5c4c6835aa7
Ray Nirola <54945029+raynirola@users.noreply.github.com>
2020-12-12 12:41:50 +0530

    flex layout to col in mobile

--- stubs/inertia/resources/js/Jetstream/DialogModal.vue
+++ stubs/inertia/resources/js/Jetstream/DialogModal.vue
@@ -12,7 +12,7 @@
             </div>
         </div>
 
-        <div class="px-6 py-4 bg-gray-100 text-right">
+        <div class="px-6 py-4 bg-gray-100 text-right flex flex-col flex-col-reverse sm:items-center sm:justify-end sm:flex-row sm:space-x-6 space-y-reverse space-y-4 sm:space-y-0">
             <slot name="footer">
             </slot>
         </div>
