9b36dd786e16b56b3081a1e358a67f5adc30a6c1
Taylor Otwell <taylorotwell@gmail.com>
2020-12-15 16:24:37 -0600

    add dusk selectors

--- stubs/inertia/resources/js/Pages/Profile/LogoutOtherBrowserSessionsForm.vue
+++ stubs/inertia/resources/js/Pages/Profile/LogoutOtherBrowserSessionsForm.vue
@@ -9,79 +9,86 @@
         </template>
 
         <template #content>
-            <div class="max-w-xl text-sm text-gray-600">
-                {{ $t('If necessary, you may logout of all of your other browser sessions across all of your devices. Some of your recent sessions are listed below; however, this list may not be exhaustive. If you feel your account has been compromised, you should also update your password.') }}
-            </div>
+            <div dusk="logout-other-browser-sessions">
+                <div class="max-w-xl text-sm text-gray-600">
+                    {{ $t('If necessary, you may logout of all of your other browser sessions across all of your devices. Some of your recent sessions are listed below; however, this list may not be exhaustive. If you feel your account has been compromised, you should also update your password.') }}
+                </div>
 
-            <!-- Other Browser Sessions -->
-            <div class="mt-5 space-y-6" v-if="sessions.length > 0">
-                <div class="flex items-center" v-for="(session, i) in sessions" :key="i">
-                    <div>
-                        <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor" class="w-8 h-8 text-gray-500" v-if="session.agent.is_desktop">
-                            <path d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>
-                        </svg>
+                <!-- Other Browser Sessions -->
+                <div class="mt-5 space-y-6" v-if="sessions.length > 0">
+                    <div class="flex items-center" v-for="(session, i) in sessions" :key="i">
+                        <div>
+                            <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor" class="w-8 h-8 text-gray-500" v-if="session.agent.is_desktop">
+                                <path d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>
+                            </svg>
 
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round" class="w-8 h-8 text-gray-500" v-else>
-                            <path d="M0 0h24v24H0z" stroke="none"></path><rect x="7" y="4" width="10" height="16" rx="1"></rect><path d="M11 5h2M12 17v.01"></path>
-                        </svg>
-                    </div>
-
-                    <div class="ml-3">
-                        <div class="text-sm text-gray-600">
-                            {{ session.agent.platform }} - {{ session.agent.browser }}
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round" class="w-8 h-8 text-gray-500" v-else>
+                                <path d="M0 0h24v24H0z" stroke="none"></path><rect x="7" y="4" width="10" height="16" rx="1"></rect><path d="M11 5h2M12 17v.01"></path>
+                            </svg>
                         </div>
 
-                        <div>
-                            <div class="text-xs text-gray-500">
-                                {{ session.ip_address }},
+                        <div class="ml-3">
+                            <div class="text-sm text-gray-600">
+                                {{ session.agent.platform }} - {{ session.agent.browser }}
+                            </div>
 
-                                <span class="text-green-500 font-semibold" v-if="session.is_current_device">{{ $t('This device') }}</span>
-                                <span v-else>{{ $t('Last active {last_active}', { 'last_active': session.last_active }) }}</span>
+                            <div>
+                                <div class="text-xs text-gray-500">
+                                    {{ session.ip_address }},
+
+                                    <span class="text-green-500 font-semibold" v-if="session.is_current_device">{{ $t('This device') }}</span>
+                                    <span v-else>{{ $t('Last active {last_active}', { 'last_active': session.last_active }) }}</span>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <div class="flex items-center mt-5">
-                <jet-button @click.native="confirmLogout">
-                    {{ $t('Logout Other Browser Sessions') }}
-                </jet-button>
-
-                <jet-action-message :on="form.recentlySuccessful" class="ml-3">
-                    {{ $t('Done.') }}
-                </jet-action-message>
-            </div>
-
-            <!-- Logout Other Devices Confirmation Modal -->
-            <jet-dialog-modal :show="confirmingLogout" @close="confirmingLogout = false">
-                <template #title>
-                    {{ $t('Logout Other Browser Sessions') }}
-                </template>
-
-                <template #content>
-                    {{ $t('Please enter your password to confirm you would like to logout of your other browser sessions across all of your devices.') }}
-
-                    <div class="mt-4">
-                        <jet-input type="password" class="mt-1 block w-3/4" :placeholder="$t('Password')"
-                                    ref="password"
-                                    v-model="form.password"
-                                    @keyup.enter.native="logoutOtherBrowserSessions" />
-
-                        <jet-input-error :message="form.error('password')" class="mt-2" />
-                    </div>
-                </template>
-
-                <template #footer>
-                    <jet-secondary-button @click.native="confirmingLogout = false">
-                        {{ $t('Nevermind') }}
-                    </jet-secondary-button>
-
-                    <jet-button class="ml-2" @click.native="logoutOtherBrowserSessions" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                <div class="flex items-center mt-5">
+                    <jet-button dusk="logout-other-browser-sessions-button" @click.native="confirmLogout">
                         {{ $t('Logout Other Browser Sessions') }}
                     </jet-button>
-                </template>
-            </jet-dialog-modal>
+
+                    <jet-action-message :on="form.recentlySuccessful" class="ml-3">
+                        {{ $t('Done.') }}
+                    </jet-action-message>
+                </div>
+
+                <!-- Logout Other Devices Confirmation Modal -->
+                <jet-dialog-modal :show="confirmingLogout" @close="confirmingLogout = false">
+                    <template #title>
+                        {{ $t('Logout Other Browser Sessions') }}
+                    </template>
+
+                    <template #content>
+                        {{ $t('Please enter your password to confirm you would like to logout of your other browser sessions across all of your devices.') }}
+
+                        <div class="mt-4">
+                            <jet-input type="password" class="mt-1 block w-3/4" :placeholder="$t('Password')"
+                                        ref="password"
+                                        dusk="logout-other-browser-sessions-confirmation-password"
+                                        v-model="form.password"
+                                        @keyup.enter.native="logoutOtherBrowserSessions" />
+
+                            <jet-input-error :message="form.error('password')" class="mt-2" />
+                        </div>
+                    </template>
+
+                    <template #footer>
+                        <jet-secondary-button @click.native="confirmingLogout = false">
+                            {{ $t('Nevermind') }}
+                        </jet-secondary-button>
+
+                        <jet-button class="ml-2"
+                                    dusk="logout-other-browser-sessions-confirmation-button"
+                                    @click.native="logoutOtherBrowserSessions"
+                                    :class="{ 'opacity-25': form.processing }"
+                                    :disabled="form.processing">
+                            {{ $t('Logout Other Browser Sessions') }}
+                        </jet-button>
+                    </template>
+                </jet-dialog-modal>
+            </div>
         </template>
     </jet-action-section>
 </template>
--- stubs/livewire/resources/views/profile/logout-other-browser-sessions-form.blade.php
+++ stubs/livewire/resources/views/profile/logout-other-browser-sessions-form.blade.php
@@ -8,87 +8,95 @@
     </x-slot>
 
     <x-slot name="content">
-        <div class="max-w-xl text-sm text-gray-600">
-            {{ __('If necessary, you may logout of all of your other browser sessions across all of your devices. Some of your recent sessions are listed below; however, this list may not be exhaustive. If you feel your account has been compromised, you should also update your password.') }}
-        </div>
+        <div dusk="logout-other-browser-sessions">
+            <div class="max-w-xl text-sm text-gray-600">
+                {{ __('If necessary, you may logout of all of your other browser sessions across all of your devices. Some of your recent sessions are listed below; however, this list may not be exhaustive. If you feel your account has been compromised, you should also update your password.') }}
+            </div>
 
-        @if (count($this->sessions) > 0)
-            <div class="mt-5 space-y-6">
-                <!-- Other Browser Sessions -->
-                @foreach ($this->sessions as $session)
-                    <div class="flex items-center">
-                        <div>
-                            @if ($session->agent->isDesktop())
-                                <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor" class="w-8 h-8 text-gray-500">
-                                    <path d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>
-                                </svg>
-                            @else
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round" class="w-8 h-8 text-gray-500">
-                                    <path d="M0 0h24v24H0z" stroke="none"></path><rect x="7" y="4" width="10" height="16" rx="1"></rect><path d="M11 5h2M12 17v.01"></path>
-                                </svg>
-                            @endif
-                        </div>
-
-                        <div class="ml-3">
-                            <div class="text-sm text-gray-600">
-                                {{ $session->agent->platform() }} - {{ $session->agent->browser() }}
+            @if (count($this->sessions) > 0)
+                <div class="mt-5 space-y-6">
+                    <!-- Other Browser Sessions -->
+                    @foreach ($this->sessions as $session)
+                        <div class="flex items-center">
+                            <div>
+                                @if ($session->agent->isDesktop())
+                                    <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor" class="w-8 h-8 text-gray-500">
+                                        <path d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>
+                                    </svg>
+                                @else
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round" class="w-8 h-8 text-gray-500">
+                                        <path d="M0 0h24v24H0z" stroke="none"></path><rect x="7" y="4" width="10" height="16" rx="1"></rect><path d="M11 5h2M12 17v.01"></path>
+                                    </svg>
+                                @endif
                             </div>
 
-                            <div>
-                                <div class="text-xs text-gray-500">
-                                    {{ $session->ip_address }},
+                            <div class="ml-3">
+                                <div class="text-sm text-gray-600">
+                                    {{ $session->agent->platform() }} - {{ $session->agent->browser() }}
+                                </div>
 
-                                    @if ($session->is_current_device)
-                                        <span class="text-green-500 font-semibold">{{ __('This device') }}</span>
-                                    @else
-                                        {{ __('Last active') }} {{ $session->last_active }}
-                                    @endif
+                                <div>
+                                    <div class="text-xs text-gray-500">
+                                        {{ $session->ip_address }},
+
+                                        @if ($session->is_current_device)
+                                            <span class="text-green-500 font-semibold">{{ __('This device') }}</span>
+                                        @else
+                                            {{ __('Last active') }} {{ $session->last_active }}
+                                        @endif
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                @endforeach
-            </div>
-        @endif
-
-        <div class="flex items-center mt-5">
-            <x-jet-button wire:click="confirmLogout" wire:loading.attr="disabled">
-                {{ __('Logout Other Browser Sessions') }}
-            </x-jet-button>
-
-            <x-jet-action-message class="ml-3" on="loggedOut">
-                {{ __('Done.') }}
-            </x-jet-action-message>
-        </div>
-
-        <!-- Logout Other Devices Confirmation Modal -->
-        <x-jet-dialog-modal wire:model="confirmingLogout">
-            <x-slot name="title">
-                {{ __('Logout Other Browser Sessions') }}
-            </x-slot>
-
-            <x-slot name="content">
-                {{ __('Please enter your password to confirm you would like to logout of your other browser sessions across all of your devices.') }}
-
-                <div class="mt-4" x-data="{}" x-on:confirming-logout-other-browser-sessions.window="setTimeout(() => $refs.password.focus(), 250)">
-                    <x-jet-input type="password" class="mt-1 block w-3/4" placeholder="{{ __('Password') }}"
-                                x-ref="password"
-                                wire:model.defer="password"
-                                wire:keydown.enter="logoutOtherBrowserSessions" />
-
-                    <x-jet-input-error for="password" class="mt-2" />
+                    @endforeach
                 </div>
-            </x-slot>
+            @endif
 
-            <x-slot name="footer">
-                <x-jet-secondary-button wire:click="$toggle('confirmingLogout')" wire:loading.attr="disabled">
-                    {{ __('Nevermind') }}
-                </x-jet-secondary-button>
-
-                <x-jet-button class="ml-2" wire:click="logoutOtherBrowserSessions" wire:loading.attr="disabled">
+            <div class="flex items-center mt-5">
+                <x-jet-button dusk="logout-other-browser-sessions-button"
+                                wire:click="confirmLogout"
+                                wire:loading.attr="disabled">
                     {{ __('Logout Other Browser Sessions') }}
                 </x-jet-button>
-            </x-slot>
-        </x-jet-dialog-modal>
+
+                <x-jet-action-message class="ml-3" on="loggedOut">
+                    {{ __('Done.') }}
+                </x-jet-action-message>
+            </div>
+
+            <!-- Logout Other Devices Confirmation Modal -->
+            <x-jet-dialog-modal wire:model="confirmingLogout">
+                <x-slot name="title">
+                    {{ __('Logout Other Browser Sessions') }}
+                </x-slot>
+
+                <x-slot name="content">
+                    {{ __('Please enter your password to confirm you would like to logout of your other browser sessions across all of your devices.') }}
+
+                    <div class="mt-4" x-data="{}" x-on:confirming-logout-other-browser-sessions.window="setTimeout(() => $refs.password.focus(), 250)">
+                        <x-jet-input type="password" class="mt-1 block w-3/4" placeholder="{{ __('Password') }}"
+                                    x-ref="password"
+                                    dusk="logout-other-browser-sessions-confirmation-password"
+                                    wire:model.defer="password"
+                                    wire:keydown.enter="logoutOtherBrowserSessions" />
+
+                        <x-jet-input-error for="password" class="mt-2" />
+                    </div>
+                </x-slot>
+
+                <x-slot name="footer">
+                    <x-jet-secondary-button wire:click="$toggle('confirmingLogout')" wire:loading.attr="disabled">
+                        {{ __('Nevermind') }}
+                    </x-jet-secondary-button>
+
+                    <x-jet-button class="ml-2"
+                                    dusk="logout-other-browser-sessions-confirmation-button"
+                                    wire:click="logoutOtherBrowserSessions"
+                                    wire:loading.attr="disabled">
+                        {{ __('Logout Other Browser Sessions') }}
+                    </x-jet-button>
+                </x-slot>
+            </x-jet-dialog-modal>
+        </div>
     </x-slot>
 </x-jet-action-section>
