cbb10a3dcadaaf5804ec5ef89a36c304b6491b26
Arnaud Lier <zeprofdecoding@gmail.com>
2020-12-16 15:15:46 +0100

    Add a <noscript> tag

--- stubs/livewire/resources/views/layouts/app.blade.php
+++ stubs/livewire/resources/views/layouts/app.blade.php
@@ -20,6 +20,23 @@
     </head>
     <body class="font-sans antialiased">
         <x-jet-banner />
+        <noscript>
+            <div class="bg-red-700">
+                <div class="max-w-screen-xl mx-auto py-2 px-3 sm:px-6 lg:px-8">
+                    <div class="flex items-center justify-between flex-wrap">
+                        <div class="w-0 flex-1 flex items-center min-w-0">
+                            <span class="flex p-2 rounded-lg bg-red-600">
+                                <svg class="h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                </svg>
+                            </span>
+
+                            <p class="ml-3 font-medium text-sm text-white truncate">Please <a href="https://www.enable-javascript.com">enable JavaScript</a> in order to visit this website.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </noscript>
 
         <div class="min-h-screen bg-gray-100">
             @livewire('navigation-menu')
