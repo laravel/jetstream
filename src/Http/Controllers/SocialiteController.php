79a6d13f069837cf68702d4f54795766a0443527
Joel Butcher <joel@joelbutcher.co.uk>
2020-12-17 11:37:22 +0000

    refactor controller logic

--- src/Http/Controllers/SocialiteController.php
+++ src/Http/Controllers/SocialiteController.php
@@ -9,16 +9,14 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Auth;
-use Laravel\Jetstream\ConnectsToSocialProvider;
 use Laravel\Jetstream\Contracts\CreatesUserFromProvider;
 use Laravel\Jetstream\Contracts\SetsUserPasswords;
+use Laravel\Jetstream\Jetstream;
 use Laravel\Socialite\Contracts\User as SocialiteUserContract;
 use Laravel\Socialite\Facades\Socialite;
 
 class SocialiteController extends Controller
 {
-    use ConnectsToSocialProvider;
-
     /**
      * The guard implementation.
      *
@@ -70,6 +68,8 @@ class SocialiteController extends Controller
      */
     public function redirectToProvider(Request $request, string $provider)
     {
+        session()->put('origin_url', back()->getTargetUrl());
+
         return Socialite::driver($provider)->redirect();
     }
 
@@ -88,49 +88,93 @@ class SocialiteController extends Controller
                 : redirect()->route('register')->withErrors($request->error_description);
         }
 
-        $providerUser = Socialite::driver($provider)->user();
+        $providerAccount = Socialite::driver($provider)->user();
 
-        $existing = ConnectedAccount::firstWhere([
-            'provider_id' => $providerUser->getId(),
+        $account = ConnectedAccount::firstWhere([
+            'provider_id' => $providerAccount->getId(),
             'provider_name' => $provider,
         ]);
 
-        if ($existing && Auth::check() && Auth::user()->id != $existing->user_id) {
-            return redirect(config('fortify.home'))->dangerBanner(
-                __('That account is already associated with a user. Please use a different account.')
+        // Authenticated...
+        if (! is_null($user = Auth::user())) {
+            if ($account && $account->user_id !== $user->id) {
+                return redirect()->route('profile.show')->dangerBanner(
+                    __('This :Provider sign in account is already associated with another user. Please try a different account.', ['provider' => $provider])
+                );
+            }
+
+            if (! $account) {
+                $this->createProviderForUser($user, $provider, $providerAccount);
+
+                return redirect()->route('profile.show')->banner(
+                    __('You have successfully connected :Provider to your account.', ['provider' => $provider])
+                );
+
+            }
+
+            return redirect()->route('profile.show')->dangerBanner(
+                __('This :Provider sign in account is already associated with your user.', ['provider' => $provider])
             );
         }
 
-        $user = $existing
-            ? $existing->user
-            : ($this->getUser($providerUser) ?? $this->createsUser->create($provider, $providerUser));
+        // Registration...
+        if (session()->get('origin_url') === route('register')) {
 
-        $this->connectToProvider($user, $provider, $providerUser);
+            if ($account) {
+                return redirect()->route('register')->withErrors(
+                    __('An account with that :Provider sign in already exists, please login.', ['provider' => $provider])
+                );
+            }
 
-        if (! Auth::check()) {
-            $this->guard->login($user);
+            if (! $providerAccount->getEmail()) {
+                return redirect()->route('register')->withErrors(
+                    __('No email address is associated with this :Provider account. Please try a different account.', ['provider' => $provider])
+                );
+            }
+
+            if (Jetstream::newUserModel()->where('email', $providerAccount->getEmail())) {
+                return redirect()->route('register')->withErrors(
+                    __('An account with that email address already exists. Please login to connect your :Provider account.', ['provider' => $provider])
+                );
+            }
+
+            $account = $this->createProviderForUser(
+                $user = $this->createsUser->create($provider, $providerAccount),
+                $provider,
+                $providerAccount
+            );
+
+            $this->guard->login($account->user);
 
             return redirect(config('fortify.home'));
         }
 
-        return redirect(config('fortify.home'))->banner(
-            __('You have successfully connected :Provider to your account.', ['provider' => $provider])
-        );
-    }
-
-    /**
-     * Find a user from a given Socialite provided user, or create a new
-     * one from the details given by the provider.
-     *
-     * @param  \Laravel\Socialite\Contracts\User  $providerUser
-     * @return \Illuminate\Contracts\Auth\Authenticatable
-     */
-    protected function getUser(SocialiteUserContract $providerUser)
-    {
-        if (Auth::check()) {
-            return Auth::user();
+        if (! $account) {
+            return redirect()->route('login')->withErrors(
+                __('An account with this :Provider sign in was not found. Please register or try a different sign in method.', ['provider' => $provider])
+            );
         }
 
-        return User::where('email', $providerUser->getEmail())->first();
+        $this->guard->login($account->user);
+
+        return redirect(config('fortify.home'));
+    }
+    /**
+     * Authenticate the user via Socialite.
+     *
+     * @param  string  $provider
+     * @param  \Laravel\Socialite\Contracts\User  $providerAccount
+     * @return \Laravel\Socialite\Contracts\User
+     */
+    protected function createProviderForUser(User $user, string $provider, SocialiteUserContract $providerAccount)
+    {
+        return $user->connectedAccounts()->create([
+            'provider_name' => strtolower($provider),
+            'provider_id' => $providerAccount->getId(),
+            'token' => $providerAccount->token,
+            'secret' => $providerAccount->tokenSecret ?? null,
+            'refresh_token' => $providerAccount->refreshToken ?? null,
+            'expires_at' => $providerAccount->expiresAt ?? null,
+        ]);
     }
 }
