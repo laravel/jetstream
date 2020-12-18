b8ae15c1e3810b7f9ea6decba36c0da03d1bce2d
Arnaud Lier <zeprofdecoding@gmail.com>
2020-12-18 10:37:03 +0100

    Update RegistrationTest.php

--- stubs/tests/RegistrationTest.php
+++ stubs/tests/RegistrationTest.php
@@ -24,6 +24,7 @@ class RegistrationTest extends TestCase
             'email' => 'test@example.com',
             'password' => 'password',
             'password_confirmation' => 'password',
+            'terms' => true,
         ]);
 
         $this->assertAuthenticated();
