e03c92491828896cce3ae23bde9d0623ca8ff124
Samuel Štancl <samuel.stancl@gmail.com>
2021-01-01 21:23:52 +0100

    Add @can check to mobile menu (#576)
    * Add @can check to mobile menu
    
    * Update navigation-menu.blade.php
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- stubs/livewire/resources/views/navigation-menu.blade.php
+++ stubs/livewire/resources/views/navigation-menu.blade.php
@@ -194,9 +194,11 @@
                         {{ __('Team Settings') }}
                     </x-jet-responsive-nav-link>
 
-                    <x-jet-responsive-nav-link href="{{ route('teams.create') }}" :active="request()->routeIs('teams.create')">
-                        {{ __('Create New Team') }}
-                    </x-jet-responsive-nav-link>
+                    @can('create', Laravel\Jetstream\Jetstream::newTeamModel())
+                        <x-jet-responsive-nav-link href="{{ route('teams.create') }}" :active="request()->routeIs('teams.create')">
+                            {{ __('Create New Team') }}
+                        </x-jet-responsive-nav-link>
+                    @endcan
 
                     <div class="border-t border-gray-200"></div>
 
