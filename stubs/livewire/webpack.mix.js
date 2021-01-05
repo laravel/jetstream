11a0fa207f08b8d1bcd9b92605deef63ccf0425e
Taylor Otwell <taylorotwell@gmail.com>
2021-01-05 09:56:48 -0600

    remove vue

--- stubs/livewire/webpack.mix.js
+++ stubs/livewire/webpack.mix.js
@@ -11,7 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js').vue()
+mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         require('postcss-import'),
         require('tailwindcss'),
