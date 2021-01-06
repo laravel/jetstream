fca50e70fea3b222e1fa334a0bd7f688aa6c7c52
Richard Kapička <richard@kapicka.cz>
2021-01-06 17:35:56 +0100

    [2.x] Banner should be hidden in default

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -9,7 +9,8 @@
                     message = event.detail.message;
                     show = true;
                 });
-            ">
+            "
+            style="display: none;">
     <div class="max-w-screen-xl mx-auto py-2 px-3 sm:px-6 lg:px-8">
         <div class="flex items-center justify-between flex-wrap">
             <div class="w-0 flex-1 flex items-center min-w-0">
