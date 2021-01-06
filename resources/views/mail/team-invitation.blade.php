47fd6bcb570b323667f7991235f7cca4d90d3d25
Oscar Kolsrud <30559109+OscarKolsrud@users.noreply.github.com>
2021-01-06 17:08:57 +0100

    Make translatable
    Made the text on the buttons in the team invitation email translatable.

--- resources/views/mail/team-invitation.blade.php
+++ resources/views/mail/team-invitation.blade.php
@@ -4,13 +4,13 @@
 {{ __('If you do not have an account, you may create one by clicking the button below. After creating an account, you may click the invitation acceptance button in this email to accept the team invitation:') }}
 
 @component('mail::button', ['url' => route('register')])
-Create Account
+{{ __('Create Account') }}
 @endcomponent
 
 {{ __('If you already have an account, you may accept this invitation by clicking the button below:') }}
 
 @component('mail::button', ['url' => $acceptUrl])
-Accept Invitation
+{{ __('Accept Invitation') }}
 @endcomponent
 
 {{ __('If you did not expect to receive an invitation to this team, you may discard this email.') }}
