e037da365ad4c1a5935e68af9c33478533997e05
Taylor Otwell <taylor@laravel.com>
2021-01-18 10:01:25 -0600

    Update DeleteAccountTest.php

--- stubs/tests/inertia/DeleteAccountTest.php
+++ stubs/tests/inertia/DeleteAccountTest.php
@@ -14,7 +14,7 @@ class DeleteAccountTest extends TestCase
     public function test_user_accounts_can_be_deleted()
     {
         if (! Features::hasAccountDeletionFeatures()) {
-            return $this->markTestSkipped('accountDeletion is not enabled.');
+            return $this->markTestSkipped('Account deletion is not enabled.');
         }
 
         $this->actingAs($user = User::factory()->create());
@@ -29,7 +29,7 @@ class DeleteAccountTest extends TestCase
     public function test_correct_password_must_be_provided_before_account_can_be_deleted()
     {
         if (! Features::hasAccountDeletionFeatures()) {
-            return $this->markTestSkipped('accountDeletion is not enabled.');
+            return $this->markTestSkipped('Account deletion is not enabled.');
         }
 
         $this->actingAs($user = User::factory()->create());
