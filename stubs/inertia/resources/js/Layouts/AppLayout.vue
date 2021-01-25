49a2949ee617fab3f66a361c8b9ed8e7f432b515
Nicolas Beauvais <2951704+nicolasbeauvais@users.noreply.github.com>
2021-01-25 15:53:41 +0100

    Hide AppLayout header if no slot defined in Page component (#650)

--- stubs/inertia/resources/js/Layouts/AppLayout.vue
+++ stubs/inertia/resources/js/Layouts/AppLayout.vue
@@ -215,7 +215,7 @@
             </nav>
 
             <!-- Page Heading -->
-            <header class="bg-white shadow">
+            <header class="bg-white shadow" v-if="$slots.header">
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                     <slot name="header"></slot>
                 </div>
