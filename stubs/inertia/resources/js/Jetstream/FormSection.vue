694ff605eb0fdbe761b5f08c934cf86f7bbd004d
altercode <p.wajcowicz@gmail.com>
2021-02-09 09:30:26 +0100

    Fix overflow hidden issue with overlaping elements inside form section

--- stubs/inertia/resources/js/Jetstream/FormSection.vue
+++ stubs/inertia/resources/js/Jetstream/FormSection.vue
@@ -7,16 +7,15 @@
 
         <div class="mt-5 md:mt-0 md:col-span-2">
             <form @submit.prevent="$emit('submitted')">
-                <div class="shadow overflow-hidden sm:rounded-md">
-                    <div class="px-4 py-5 bg-white sm:p-6">
-                        <div class="grid grid-cols-6 gap-6">
-                            <slot name="form"></slot>
-                        </div>
+                <div class="px-4 py-5 bg-white sm:p-6 shadow"
+                    :class="hasActions ? 'sm:rounded-tl-md sm:rounded-tr-md' : 'sm:rounded-md'">
+                    <div class="grid grid-cols-6 gap-6">
+                        <slot name="form"></slot>
                     </div>
+                </div>
 
-                    <div class="flex items-center justify-end px-4 py-3 bg-gray-50 text-right sm:px-6" v-if="hasActions">
-                        <slot name="actions"></slot>
-                    </div>
+                <div class="flex items-center justify-end px-4 py-3 bg-gray-50 text-right sm:px-6 shadow sm:rounded-bl-md sm:rounded-br-md" v-if="hasActions">
+                    <slot name="actions"></slot>
                 </div>
             </form>
         </div>
