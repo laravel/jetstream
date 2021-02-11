d5dcabf35204bf3dd4420f11458b1fcef216472a
Vaggelis Yfantis <me@octoper.me>
2021-02-11 12:14:12 +0200

    Fixed StyleCI Warnings

--- stubs/inertia/resources/js/app.js
+++ stubs/inertia/resources/js/app.js
@@ -2,7 +2,7 @@ require('./bootstrap');
 
 // Import modules...
 import { createApp, h } from 'vue';
-import { App as InertiaApp, plugin as InertiaPlugin }from '@inertiajs/inertia-vue3';
+import { App as InertiaApp, plugin as InertiaPlugin } from '@inertiajs/inertia-vue3';
 
 const el = document.getElementById('app');
 
@@ -10,7 +10,7 @@ createApp({
     render: () =>
         h(InertiaApp, {
             initialPage: JSON.parse(el.dataset.page),
-            resolveComponent: name => require(`./Pages/${name}`).default,
+            resolveComponent: (name) => require(`./Pages/${name}`).default,
         }),
 })
     .mixin({ methods: { route } })
