1cc27a56e96f1c5211119591b4b5870d787391cd
Jimmie Johansson <jimmitjoo@gmail.com>
2021-02-12 09:43:52 +0100

    translate validation errors

--- resources/views/components/validation-errors.blade.php
+++ resources/views/components/validation-errors.blade.php
@@ -4,7 +4,7 @@
 
         <ul class="mt-3 list-disc list-inside text-sm text-red-600">
             @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
+                <li>{{ __($error) }}</li>
             @endforeach
         </ul>
     </div>
