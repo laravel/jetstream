c1f1fa0ab964f3b9dbe144eb60e18d38e835b195
Taylor Otwell <taylorotwell@gmail.com>
2021-02-18 15:40:11 -0600

    use inertia progress

--- stubs/inertia/resources/js/app.js
+++ stubs/inertia/resources/js/app.js
@@ -3,6 +3,7 @@ require('./bootstrap');
 // Import modules...
 import { createApp, h } from 'vue';
 import { App as InertiaApp, plugin as InertiaPlugin } from '@inertiajs/inertia-vue3';
+import { InertiaProgress } from '@inertiajs/progress';
 
 const el = document.getElementById('app');
 
@@ -16,3 +17,5 @@ createApp({
     .mixin({ methods: { route } })
     .use(InertiaPlugin)
     .mount(el);
+
+InertiaProgress.init({ color: '#4B5563' });
