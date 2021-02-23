debf79d278ac7b2fddfc5ae949adcf2b740a4258
Elvaris Gjinali <elvaris.gjinali@gmail.com>
2021-02-23 14:35:33 +0100

    vue3 v-on native deprecated directive removed (#689)
    vue3 v-on native deprecated directive has been removed

--- stubs/inertia/resources/js/Pages/Profile/UpdateProfileInformationForm.vue
+++ stubs/inertia/resources/js/Pages/Profile/UpdateProfileInformationForm.vue
@@ -30,11 +30,11 @@
                     </span>
                 </div>
 
-                <jet-secondary-button class="mt-2 mr-2" type="button" @click.native.prevent="selectNewPhoto">
+                <jet-secondary-button class="mt-2 mr-2" type="button" @click.prevent="selectNewPhoto">
                     Select A New Photo
                 </jet-secondary-button>
 
-                <jet-secondary-button type="button" class="mt-2" @click.native.prevent="deletePhoto" v-if="user.profile_photo_path">
+                <jet-secondary-button type="button" class="mt-2" @click.prevent="deletePhoto" v-if="user.profile_photo_path">
                     Remove Photo
                 </jet-secondary-button>
 
