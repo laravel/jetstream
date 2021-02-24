90b6d1b5ddb02a10f4698428e02c204962a9d16a
Taylor Otwell <taylorotwell@gmail.com>
2021-02-24 07:59:57 -0600

    update mimes

--- stubs/app/Actions/Fortify/UpdateUserProfileInformation.php
+++ stubs/app/Actions/Fortify/UpdateUserProfileInformation.php
@@ -21,7 +21,7 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
         Validator::make($input, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'email', 'max:255', Rule::unique('users')->ignore($user->id)],
-            'photo' => ['nullable', 'image', 'max:1024'],
+            'photo' => ['nullable', 'mimes:jpg,jpeg,png', 'max:1024'],
         ])->validateWithBag('updateProfileInformation');
 
         if (isset($input['photo'])) {
