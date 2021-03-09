ce2f6b02458428426c4b35f3393e42f11a7d47c8
hinnerk <gnutzmann@flexponsive.net>
2021-03-09 21:05:00 +0100

    templates should wrap mix call inside url() - fix for inertia stack

--- stubs/inertia/resources/views/app.blade.php
+++ stubs/inertia/resources/views/app.blade.php
@@ -11,11 +11,11 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <link rel="stylesheet" href="{{ url(mix('css/app.css')) }}">
 
         <!-- Scripts -->
         @routes
-        <script src="{{ mix('js/app.js') }}" defer></script>
+        <script src="{{ url(mix('js/app.js')) }}" defer></script>
     </head>
     <body class="font-sans antialiased">
         @inertia
