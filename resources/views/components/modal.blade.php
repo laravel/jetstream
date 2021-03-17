3a4feabace9aeb0efdb6be9d4a14beaf0e35305d
Pedro Henrique <38913462+PH7-Jack@users.noreply.github.com>
2021-03-17 13:19:25 -0300

    change watch to bind class
    - removed style because does not has effect
    - change watch to :class

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -12,7 +12,8 @@ $maxWidth = [
 ][$maxWidth ?? '2xl'];
 @endphp
 
-<div
+<div class="jetstream-modal fixed inset-0 overflow-y-auto px-4 py-6 sm:px-0 z-50"
+    :class="{ 'overflow-y-hidden': show }"
     x-data="{
         show: @entangle($attributes->wire('model')).defer,
         focusables() {
@@ -30,21 +31,12 @@ $maxWidth = [
         nextFocusableIndex() { return (this.focusables().indexOf(document.activeElement) + 1) % (this.focusables().length + 1) },
         prevFocusableIndex() { return Math.max(0, this.focusables().indexOf(document.activeElement)) -1 },
     }"
-    x-init="$watch('show', value => {
-        if (value) {
-            document.body.classList.add('overflow-y-hidden');
-        } else {
-            document.body.classList.remove('overflow-y-hidden');
-        }
-    })"
     x-on:close.stop="show = false"
     x-on:keydown.escape.window="show = false"
     x-on:keydown.tab.prevent="$event.shiftKey || nextFocusable().focus()"
     x-on:keydown.shift.tab.prevent="prevFocusable().focus()"
     x-show="show"
     id="{{ $id }}"
-    class="jetstream-modal fixed inset-0 overflow-y-auto px-4 py-6 sm:px-0 z-50"
-    style="display: none;"
 >
     <div x-show="show" class="fixed inset-0 transform transition-all" x-on:click="show = false" x-transition:enter="ease-out duration-300"
                     x-transition:enter-start="opacity-0"
