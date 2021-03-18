bde56ba00eaf3e0a430aadc6660b1f2f9b39980f
Brian Dillingham <brian@dillingham.dev>
2021-03-18 09:14:52 -0400

    Add a tag dropdown link (#711)

--- stubs/inertia/resources/js/Jetstream/DropdownLink.vue
+++ stubs/inertia/resources/js/Jetstream/DropdownLink.vue
@@ -3,6 +3,10 @@
         <button type="submit" class="block w-full px-4 py-2 text-sm leading-5 text-gray-700 text-left hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out" v-if="as == 'button'">
             <slot></slot>
         </button>
+        
+        <a :href="href" class="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out" v-else-if="as =='a'">
+            <slot></slot>
+        </a>
 
         <inertia-link :href="href" class="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out" v-else>
             <slot></slot>
