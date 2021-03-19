9c68ff5b09e17d170954a2d10392fe7c6dcbb9a9
Brian Dillingham <brian@dillingham.dev>
2021-03-19 14:03:18 -0400

    Add href button for inertia component

--- stubs/inertia/resources/js/Jetstream/Button.vue
+++ stubs/inertia/resources/js/Jetstream/Button.vue
@@ -1,5 +1,8 @@
 <template>
-    <button :type="type" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150">
+    <a :href="href" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray transition ease-in-out duration-150" v-if="href">
+        <slot></slot>
+    </a>
+    <button :type="type" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray transition ease-in-out duration-150" v-else>
         <slot></slot>
     </button>
 </template>
@@ -11,6 +14,10 @@
                 type: String,
                 default: 'submit',
             },
+            href: {
+                type: String,
+                default: null
+            }
         }
     }
 </script>
