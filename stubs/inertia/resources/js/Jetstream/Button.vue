c417cc0f70ded6afd4ed2dd24375cb90c0070be5
Brian Dillingham <brian@dillingham.dev>
2021-03-19 14:18:28 -0400

    Add inertia-link condition

--- stubs/inertia/resources/js/Jetstream/Button.vue
+++ stubs/inertia/resources/js/Jetstream/Button.vue
@@ -1,7 +1,10 @@
 <template>
-    <a :href="href" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray transition ease-in-out duration-150" v-if="href">
+    <a :href="href" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray transition ease-in-out duration-150" v-if="href && as == 'a'">
         <slot></slot>
     </a>
+    <inertia-link :href="href" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray transition ease-in-out duration-150" v-else-if="href">
+        <slot></slot>
+    </inertia-link>
     <button :type="type" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray transition ease-in-out duration-150" v-else>
         <slot></slot>
     </button>
@@ -14,6 +17,10 @@
                 type: String,
                 default: 'submit',
             },
+            as: {
+                type: String,
+                default: 'button'
+            }
             href: {
                 type: String,
                 default: null
