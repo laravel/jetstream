c9dffd814c4ec9a418de254ff907118c182679c5
Juno <ongvoon1988@gmail.com>
2021-03-20 17:22:19 +0800

    Livewire - mobile menu transition

--- stubs/livewire/resources/views/navigation-menu.blade.php
+++ stubs/livewire/resources/views/navigation-menu.blade.php
@@ -136,7 +136,7 @@
     </div>
 
     <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
+    <div class="relative overflow-hidden transition-all duration-700 sm:hidden max-h-0" x-ref="responsiveNavMenu" x-bind:style="open ? 'max-height: ' + $refs.responsiveNavMenu.scrollHeight + 'px' : ''">
         <div class="pt-2 pb-3 space-y-1">
             <x-jet-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
