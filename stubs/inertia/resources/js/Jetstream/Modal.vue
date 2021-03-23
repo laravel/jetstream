975f1055493914b8f4e31e033bf2593b32eb4a58
Brian Dillingham <brian@dillingham.dev>
2021-03-22 23:11:07 -0400

    Load modal slot on show

--- stubs/inertia/resources/js/Jetstream/Modal.vue
+++ stubs/inertia/resources/js/Jetstream/Modal.vue
@@ -20,7 +20,7 @@
                         leave-from-class="opacity-100 translate-y-0 sm:scale-100"
                         leave-to-class="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
                     <div v-show="show" class="mb-6 bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:w-full sm:mx-auto" :class="maxWidthClass">
-                        <slot></slot>
+                        <slot v-if="show"></slot>
                     </div>
                 </transition>
             </div>
