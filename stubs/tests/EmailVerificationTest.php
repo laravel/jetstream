e73e184b6d97e9b98cf78d69f95c4906c9aece8b
Rigel Kent Carbonel <rigel20.kent@gmail.com>
2021-03-27 21:54:27 +0800

    Fixed the test that's failing when the emailVerification feature is enabled the team feature is enabled.

--- stubs/tests/EmailVerificationTest.php
+++ stubs/tests/EmailVerificationTest.php
@@ -9,6 +9,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\URL;
 use Laravel\Fortify\Features;
+use Laravel\Jetstream\Features as JetstreamFeatures;
 use Tests\TestCase;
 
 class EmailVerificationTest extends TestCase
@@ -21,9 +22,15 @@ class EmailVerificationTest extends TestCase
             return $this->markTestSkipped('Email verification not enabled.');
         }
 
-        $user = User::factory()->create([
-            'email_verified_at' => null,
-        ]);
+        if (JetstreamFeatures::hasTeamFeatures()) {
+            $user = User::factory()->withPersonalTeam()->create([
+                'email_verified_at' => null,
+            ]);
+        } else {
+            $user = User::factory()->create([
+                'email_verified_at' => null,
+            ]);
+        }
 
         $response = $this->actingAs($user)->get('/email/verify');
 
