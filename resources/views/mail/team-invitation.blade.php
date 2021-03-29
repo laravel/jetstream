1726003e00e0436629e0894e33c8d58ecd641f3c
Kaj <kajgiesbers@hotmail.com>
2021-03-29 21:54:17 +0200

    - Fixes an error when sending email invitations when Fortify's registration feature is turned off (#741)
    Co-authored-by: Kaj Giesbers <kajgiesbers@gmail.com>

--- resources/views/mail/team-invitation.blade.php
+++ resources/views/mail/team-invitation.blade.php
@@ -1,6 +1,7 @@
 @component('mail::message')
 {{ __('You have been invited to join the :team team!', ['team' => $invitation->team->name]) }}
 
+@if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::registration()))
 {{ __('If you do not have an account, you may create one by clicking the button below. After creating an account, you may click the invitation acceptance button in this email to accept the team invitation:') }}
 
 @component('mail::button', ['url' => route('register')])
@@ -8,6 +9,11 @@
 @endcomponent
 
 {{ __('If you already have an account, you may accept this invitation by clicking the button below:') }}
+
+@else
+{{ __('You may accept this invitation by clicking the button below:') }}
+@endif
+
 
 @component('mail::button', ['url' => $acceptUrl])
 {{ __('Accept Invitation') }}
