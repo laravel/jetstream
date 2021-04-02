e3dbabbfd98e6fd816e5ad9457d38cd1d52954f4
Dominik Zarsky <43666286+Loupeznik@users.noreply.github.com>
2021-04-02 17:07:07 +0200

    Update dummy email
    Update dummy email for the test as the original email clashes with Livewire's ProfileInformationTest resulting in this test's failure

--- stubs/tests/RegistrationTest.php
+++ stubs/tests/RegistrationTest.php
@@ -22,7 +22,7 @@ class RegistrationTest extends TestCase
     {
         $response = $this->post('/register', [
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'testuser@example.com',
             'password' => 'password',
             'password_confirmation' => 'password',
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature(),
