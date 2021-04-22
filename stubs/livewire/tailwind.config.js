f37d08ef094f1a66c7fbbdfc2412d67b55929731
Julien Issler <julien@issler.net>
2021-04-22 19:42:41 +0200

    Update tailwind.config.js
    should not watch './storage/framework/views/*.php' as these are cached files

--- stubs/livewire/tailwind.config.js
+++ stubs/livewire/tailwind.config.js
@@ -4,7 +4,6 @@ module.exports = {
     purge: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './vendor/laravel/jetstream/**/*.blade.php',
-        './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
     ],
 
