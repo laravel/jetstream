c4f9a8a1976599dc1245fef5a51a9836d056a7d0
Roger Vilà <rogervila@me.com>
2021-04-28 23:22:49 +0200

    Update login.blade.php

--- stubs/livewire/resources/views/auth/login.blade.php
+++ stubs/livewire/resources/views/auth/login.blade.php
@@ -32,7 +32,11 @@
                 </label>
             </div>
 
-            <div class="flex items-center justify-end mt-4">
+            <div class="flex items-center justify-between mt-4">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
+                    {{ __('Not registered yet?') }}
+                </a>
+
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
