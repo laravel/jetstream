5a358b7ec6d57620de89531f82d02c4af31cbcfb
Roger Vilà <rogervila@me.com>
2021-04-28 23:21:17 +0200

    Update Login.vue

--- stubs/inertia/resources/js/Pages/Auth/Login.vue
+++ stubs/inertia/resources/js/Pages/Auth/Login.vue
@@ -28,7 +28,11 @@
                 </label>
             </div>
 
-            <div class="flex items-center justify-end mt-4">
+            <div class="flex items-center justify-between mt-4">
+                <inertia-link :href="route('register')" class="underline text-sm text-gray-600 hover:text-gray-900">
+                    Not registered yet?
+                </inertia-link>
+
                 <inertia-link v-if="canResetPassword" :href="route('password.request')" class="underline text-sm text-gray-600 hover:text-gray-900">
                     Forgot your password?
                 </inertia-link>
