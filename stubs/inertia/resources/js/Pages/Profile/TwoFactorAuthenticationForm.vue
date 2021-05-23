61ff3e8ec07234a88d411544c17aa02b62981768
Dante <35383529+srdante@users.noreply.github.com>
2021-05-23 00:11:08 -0300

    Update TwoFactorAuthenticationForm.vue

--- stubs/inertia/resources/js/Pages/Profile/TwoFactorAuthenticationForm.vue
+++ stubs/inertia/resources/js/Pages/Profile/TwoFactorAuthenticationForm.vue
@@ -31,7 +31,7 @@
                         </p>
                     </div>
 
-                    <div class="mt-4 dark:p-4 dark:w-56 dark:bg-white" v-html="qrCode">
+                    <div class="mt-4" v-html="qrCode">
                     </div>
                 </div>
 
