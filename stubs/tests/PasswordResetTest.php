470e52f8f04d7e57f7d48b08dbf0fb11bd752bcb
Taylor Otwell <taylorotwell@gmail.com>
2021-05-28 08:36:24 -0700

    formatting

--- stubs/tests/PasswordResetTest.php
+++ stubs/tests/PasswordResetTest.php
@@ -16,7 +16,7 @@ class PasswordResetTest extends TestCase
     public function test_reset_password_link_screen_can_be_rendered()
     {
         if (! Features::enabled(Features::updatePasswords())) {
-            return $this->markTestSkipped('Update password is not enabled.');
+            return $this->markTestSkipped('Password updates are not enabled.');
         }
 
         $response = $this->get('/forgot-password');
@@ -27,7 +27,7 @@ class PasswordResetTest extends TestCase
     public function test_reset_password_link_can_be_requested()
     {
         if (! Features::enabled(Features::updatePasswords())) {
-            return $this->markTestSkipped('Update password is not enabled.');
+            return $this->markTestSkipped('Password updates are not enabled.');
         }
 
         Notification::fake();
@@ -44,7 +44,7 @@ class PasswordResetTest extends TestCase
     public function test_reset_password_screen_can_be_rendered()
     {
         if (! Features::enabled(Features::updatePasswords())) {
-            return $this->markTestSkipped('Update password is not enabled.');
+            return $this->markTestSkipped('Password updates are not enabled.');
         }
 
         Notification::fake();
@@ -67,7 +67,7 @@ class PasswordResetTest extends TestCase
     public function test_password_can_be_reset_with_valid_token()
     {
         if (! Features::enabled(Features::updatePasswords())) {
-            return $this->markTestSkipped('Update password is not enabled.');
+            return $this->markTestSkipped('Password updates are not enabled.');
         }
 
         Notification::fake();
