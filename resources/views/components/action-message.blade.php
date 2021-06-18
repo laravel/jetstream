28b235c6c6c0ca15987bdc9873ea05a37dce6691
Caleb Porzio <calebporzio@gmail.com>
2021-06-18 12:20:01 -0400

    Upgrade Alpine from V2 to V3 (#808)

--- resources/views/components/action-message.blade.php
+++ resources/views/components/action-message.blade.php
@@ -2,7 +2,8 @@
 
 <div x-data="{ shown: false, timeout: null }"
     x-init="@this.on('{{ $on }}', () => { clearTimeout(timeout); shown = true; timeout = setTimeout(() => { shown = false }, 2000);  })"
-    x-show.transition.opacity.out.duration.1500ms="shown"
+    x-show="shown"
+    x-transition:leave.opacity.duration.1500ms
     style="display: none;"
     {{ $attributes->merge(['class' => 'text-sm text-gray-600']) }}>
     {{ $slot->isEmpty() ? 'Saved.' : $slot }}
