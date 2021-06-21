7f7c153dd0cf917fdc059cc370f7b99e4390fed9
sotten <44020272+sotten@users.noreply.github.com>
2021-06-21 15:40:54 +0200

    Tailwind JIT-Mode (#810)
    * change Tailwind to jit
    
    * change Tailwind to jit

--- stubs/livewire/tailwind.config.js
+++ stubs/livewire/tailwind.config.js
@@ -1,6 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
+    mode: 'jit',
     purge: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './vendor/laravel/jetstream/**/*.blade.php',
