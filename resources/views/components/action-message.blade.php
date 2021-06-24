d2129632a7e15b4ecbbda2880514a8311d54700d
Caleb Porzio <calebporzio@gmail.com>
2021-06-24 09:39:16 -0400

    Support both Alpine V2 and V3 (#813)

--- resources/views/components/action-message.blade.php
+++ resources/views/components/action-message.blade.php
@@ -2,7 +2,7 @@
 
 <div x-data="{ shown: false, timeout: null }"
     x-init="@this.on('{{ $on }}', () => { clearTimeout(timeout); shown = true; timeout = setTimeout(() => { shown = false }, 2000);  })"
-    x-show="shown"
+    x-show.transition.out.opacity.duration.1500ms="shown"
     x-transition:leave.opacity.duration.1500ms
     style="display: none;"
     {{ $attributes->merge(['class' => 'text-sm text-gray-600']) }}>
