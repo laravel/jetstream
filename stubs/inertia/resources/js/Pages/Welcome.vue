b055543a67dd953b9e0cfdf8a055f90e4dc1f9e6
Rahul Khimsuriya <rahulkhimsuriya38@gmail.com>
2021-07-17 12:59:51 +0530

    redirect by route name

--- stubs/inertia/resources/js/Pages/Welcome.vue
+++ stubs/inertia/resources/js/Pages/Welcome.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
         <div v-if="canLogin" class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-            <inertia-link v-if="$page.props.user" href="/dashboard" class="text-sm text-gray-700 underline">
+            <inertia-link v-if="$page.props.user" :href="route('dashboard')" class="text-sm text-gray-700 underline">
                 Dashboard
             </inertia-link>
 
