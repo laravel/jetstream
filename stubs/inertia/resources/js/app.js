05d506ca63ddb4f4353f881d27d47e3b81e9a74f
Taylor Otwell <taylorotwell@users.noreply.github.com>
2021-07-20 10:28:43 +0000

    Apply fixes from StyleCI

--- stubs/inertia/resources/js/app.js
+++ stubs/inertia/resources/js/app.js
@@ -7,12 +7,12 @@ import { InertiaProgress } from '@inertiajs/progress';
 // By default, a fresh Laravel Jetstream application has the Application Name configured as the title tag.
 // Let's grab this before Inertia gets a chance to overwrite it, and store it's value for later usage.
 // When this isn't available, we'll instead use the static value of 'Laravel'.
-const appName = window.document.getElementsByTagName('title')[0]?.innerText  || 'Laravel'
+const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
 // Next, we'll initialize our Inertia.js client-side application, for which the
 // documentation can be found at https://inertiajs.com/client-side-setup
 createInertiaApp({
-    title: title => `${title} - ${appName}`,
+    title: (title) => `${title} - ${appName}`,
     resolve: (name) => require(`./Pages/${name}.vue`),
     setup({ el, app, props, plugin }) {
         return createApp({ render: () => h(app, props) })
