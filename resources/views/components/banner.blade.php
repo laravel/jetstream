f90edd9cb1802bc475008361204d5b2c52d5ebf9
Bilfeldt <abilfeldt@gmail.com>
2021-08-16 12:22:14 +0200

    Use same styling as 'secondary-button'

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -30,7 +30,7 @@
             
             @if ($linkUrl)
                 <div class="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
-                    <a href="{{ $linkUrl }}" class="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-indigo-50">
+                    <a href="{{ $linkUrl }}" class="inline-flex items-center px-4 py-2 bg-white border border-gray-300 rounded-md font-semibold text-xs text-gray-700 uppercase tracking-widest shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:ring focus:ring-blue-200 active:text-gray-800 active:bg-gray-50 disabled:opacity-25 transition">
                         {{ $linkText ?? __('Read more') }}
                     </a>
                 </div>
