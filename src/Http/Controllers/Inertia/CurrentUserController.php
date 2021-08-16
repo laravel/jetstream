976b80f3cd6a4fbfb2b29d608f24e3cde7c4755c
Maarten Buis <buismaarten@outlook.com>
2021-08-16 15:24:48 +0200

    Removed unused imports (#856)
    * Update ApiTokenController.php
    
    * Update CurrentUserController.php
    
    * Update OtherBrowserSessionsController.php
    
    * Update Features.php
    
    * Update EmailVerificationTest.php

--- src/Http/Controllers/Inertia/CurrentUserController.php
+++ src/Http/Controllers/Inertia/CurrentUserController.php
@@ -5,7 +5,6 @@ namespace Laravel\Jetstream\Http\Controllers\Inertia;
 use Illuminate\Contracts\Auth\StatefulGuard;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 use Inertia\Inertia;
 use Laravel\Fortify\Actions\ConfirmPassword;
