b5dc1bdccecfdcb9665d05ccbd38ce59468d5eb2
Bilfeldt <abilfeldt@gmail.com>
2021-08-16 10:04:18 +0200

    Update banner component svg icon
    Currently the banner component always show a success icon (`check-circle`) regardless of the style applied for the component.
    
    This PR simply changes this so that the current success icon (`check-circle`) is only used when the component style is `success` whereas a more neutral information icon (`information-circle`) is used otherwise (meaning for `style='danger'` or any other custom styles added).

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -15,8 +15,11 @@
         <div class="flex items-center justify-between flex-wrap">
             <div class="w-0 flex-1 flex items-center min-w-0">
                 <span class="flex p-2 rounded-lg" :class="{ 'bg-indigo-600': style == 'success', 'bg-red-600': style == 'danger' }">
-                    <svg class="h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <svg x-show="style == 'success'" class="h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                    <svg x-show="style != 'success'" class="h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
                 </span>
 
