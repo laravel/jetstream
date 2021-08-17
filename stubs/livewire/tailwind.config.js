d320cdc577393d21033e949c1b8cee2020c9d64d
he11yeah <he11yeah@users.noreply.github.com>
2021-08-17 12:34:30 -0500

    #859 Target styles in default views folder
    Added code targeting './resources/views/*.blade.php' so styles won't be purged by tailwind

--- stubs/livewire/tailwind.config.js
+++ stubs/livewire/tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
         './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './resources/views/*.blade.php',
     ],
 
     theme: {
