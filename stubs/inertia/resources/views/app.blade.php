b7070ad6035f8ffa1f0a87ea2f1d645fb5e83440
Keith Brink <keith.brink@gmail.com>
2021-09-10 11:26:32 +0300

    Remove csrf_token from meta in Inertia app
    Including the `csrf_token` as a meta element conflicts with Inertia's built-in CSRF protection: https://inertiajs.com/csrf-protection#top
    
    https://a.cl.ly/Wnu0mBmA

--- stubs/inertia/resources/views/app.blade.php
+++ stubs/inertia/resources/views/app.blade.php
@@ -3,7 +3,6 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
