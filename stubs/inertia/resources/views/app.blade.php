9af39c6f4390b12851c83ffc88294550549171da
Daniel Speichert <daniel@speichert.pl>
2021-09-15 23:29:23 -0400

    Revert "fix cors policy errors with inertia stack (#797)"
    Attempting to load browser-sync-client.js without it being used in
    Jetstream by default results in blocked JS execution until timeout.

--- stubs/inertia/resources/views/app.blade.php
+++ stubs/inertia/resources/views/app.blade.php
@@ -18,9 +18,5 @@
     </head>
     <body class="font-sans antialiased">
         @inertia
-
-        @env ('local')
-            <script src="http://localhost:3000/browser-sync/browser-sync-client.js"></script>
-        @endenv
     </body>
 </html>
