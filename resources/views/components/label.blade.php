4dfee4c4f4a6d491808c534627733b563cd7d44d
Abdulaziz Saad <bigzooooz@hotmail.com>
2021-10-21 13:22:00 +0300

    required attribute for input label
    add red asterisk next to label to indicate requirement

--- resources/views/components/label.blade.php
+++ resources/views/components/label.blade.php
@@ -1,5 +1,5 @@
-@props(['value'])
+@props(['value','required' => false])
 
 <label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700']) }}>
-    {{ $value ?? $slot }}
+    {{ $value ?? $slot }} @if($required) <span class="text-red-700">*</span> @endif
 </label>
