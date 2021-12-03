ce4b35ef395c9ab2122a68b1d1b7cff7bdffce23
Victor Yoalli <victoryoalli@gmail.com>
2021-12-03 15:55:27 -0600

    Change http://localhost:3000 to the APP_URL
    When working on a custom domain name like when using Valet is a better option to have only one place to change the application url.

--- stubs/inertia/resources/views/app.blade.php
+++ stubs/inertia/resources/views/app.blade.php
@@ -20,7 +20,7 @@
         @inertia
 
         @env ('local')
-            <script src="http://localhost:3000/browser-sync/browser-sync-client.js"></script>
+            <script src="{{ config('app.url', 'http://localhost') }}:3000/browser-sync/browser-sync-client.js"></script>
         @endenv
     </body>
 </html>
