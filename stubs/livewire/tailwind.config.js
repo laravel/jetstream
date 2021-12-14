ffb290afb288ac51124220db13d9e8fbd86ef89d
Tim Geisendoerfer <geisi@users.noreply.github.com>
2021-12-14 15:36:28 +0100

    remove just in time mode from tailwind config (#926)

--- stubs/livewire/tailwind.config.js
+++ stubs/livewire/tailwind.config.js
@@ -1,7 +1,6 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-    mode: 'jit',
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './vendor/laravel/jetstream/**/*.blade.php',
