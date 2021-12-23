7fc032c8aa5766248b7da7e36a52554a6432032e
Ravi Patel <ravibpatel@users.noreply.github.com>
2021-12-23 22:20:10 +0530

    Fixed button spacing issue in dialog modal footer. (#930)

--- stubs/inertia/resources/js/Jetstream/DialogModal.vue
+++ stubs/inertia/resources/js/Jetstream/DialogModal.vue
@@ -12,7 +12,7 @@
             </div>
         </div>
 
-        <div class="px-6 py-4 bg-gray-100 text-right">
+        <div class="flex flex-row justify-end px-6 py-4 bg-gray-100 text-right">
             <slot name="footer">
             </slot>
         </div>
