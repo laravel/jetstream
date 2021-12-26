5d4168e0c9dbd41d2ac4b323c8b9c78f3fce4de7
Mohammed-Yousuf Hamid <32526267+mohammedyh@users.noreply.github.com>
2021-12-26 20:22:10 +0000

    Fixes spelling in Authentication Pest Test (#932)
    Changes:
    
    From
    - 'users can not authenticate with invalid_password'
    
    To
    - 'users cannot authenticate with invalid password'

--- stubs/pest-tests/AuthenticationTest.php
+++ stubs/pest-tests/AuthenticationTest.php
@@ -21,7 +21,7 @@ test('users can authenticate using the login screen', function () {
     $response->assertRedirect(RouteServiceProvider::HOME);
 });
 
-test('users can not authenticate with invalid_password', function () {
+test('users cannot authenticate with invalid password', function () {
     $user = User::factory()->create();
 
     $this->post('/login', [
