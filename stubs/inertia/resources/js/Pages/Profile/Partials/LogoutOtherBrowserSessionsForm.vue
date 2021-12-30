25e63876b753d67e9864ca73955471c2c0be1784
Jon H <jon@jonhassall.com>
2021-12-30 04:50:49 +0000

    Show 'Unknown' instead of 'false' if other browser session useragent details are not matched in library

--- stubs/inertia/resources/js/Pages/Profile/Partials/LogoutOtherBrowserSessionsForm.vue
+++ stubs/inertia/resources/js/Pages/Profile/Partials/LogoutOtherBrowserSessionsForm.vue
@@ -28,7 +28,7 @@
 
                     <div class="ml-3">
                         <div class="text-sm text-gray-600">
-                            {{ session.agent.platform }} - {{ session.agent.browser }}
+                            {{ session.agent.platform ? session.agent.platform : 'Unknown' }} - {{ session.agent.browser ? session.agent.browser : 'Unknown' }}
                         </div>
 
                         <div>
