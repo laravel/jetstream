ee5fc64366ca9c1f1c386a5df85d4223ae8558bd
Jon H <jon@jonhassall.com>
2021-12-30 04:58:13 +0000

    Show 'Unknown' instead of 'false' if other browser session useragent details are not matched in library (Livewire)

--- stubs/livewire/resources/views/profile/logout-other-browser-sessions-form.blade.php
+++ stubs/livewire/resources/views/profile/logout-other-browser-sessions-form.blade.php
@@ -31,7 +31,7 @@
 
                         <div class="ml-3">
                             <div class="text-sm text-gray-600">
-                                {{ $session->agent->platform() }} - {{ $session->agent->browser() }}
+                                {{ $session->agent->platform() ? $session->agent->platform() : 'Unknown' }} - {{ $session->agent->browser() ? $session->agent->browser() : 'Unknown' }}
                             </div>
 
                             <div>
