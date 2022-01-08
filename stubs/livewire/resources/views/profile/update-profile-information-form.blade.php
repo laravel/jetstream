447b306801d2e240b539aaa434285460765993e6
titonova <41286822+titonova@users.noreply.github.com>
2022-01-08 20:19:10 +0100

    Changed profile preview generation from raw JS to Alpine
    Looks  much more cleaner and tider now :-)

--- stubs/livewire/resources/views/profile/update-profile-information-form.blade.php
+++ stubs/livewire/resources/views/profile/update-profile-information-form.blade.php
@@ -14,28 +14,13 @@
                 <!-- Profile Photo File Input -->
                 <input type="file" class="hidden"
                             wire:model="photo"
-                            x-ref="photo"
-                            x-on:change="
-                                    photoName = $refs.photo.files[0].name;
-                                    const reader = new FileReader();
-                                    reader.onload = (e) => {
-                                        photoPreview = e.target.result;
-                                    };
-                                    reader.readAsDataURL($refs.photo.files[0]);
-                            " />
+                            x-ref="photo"/>
 
                 <x-jet-label for="photo" value="{{ __('Photo') }}" />
 
                 <!-- Current Profile Photo -->
-                <div class="mt-2" x-show="! photoPreview">
-                    <img src="{{ $this->user->profile_photo_url }}" alt="{{ $this->user->name }}" class="rounded-full h-20 w-20 object-cover">
-                </div>
-
-                <!-- New Profile Photo Preview -->
-                <div class="mt-2" x-show="photoPreview" style="display: none;">
-                    <span class="block rounded-full w-20 h-20 bg-cover bg-no-repeat bg-center"
-                          x-bind:style="'background-image: url(\'' + photoPreview + '\');'">
-                    </span>
+                <div class="mt-2">
+                    <img src="{{ $photo?->temporaryUrl() ?? $this->user->profile_photo_url }}" alt="{{ $this->user->name }}"  class="rounded-full h-20 w-20 object-cover" x-ref="profilePhoto">
                 </div>
 
                 <x-jet-secondary-button class="mt-2 mr-2" type="button" x-on:click.prevent="$refs.photo.click()">
