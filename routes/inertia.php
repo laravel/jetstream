e0e9eae8c406f25259761778f67b361923159525
Matthew Myers <mxm1070@users.noreply.github.com>
2022-01-15 02:34:13 -0500

    Update inertia.php

--- routes/inertia.php
+++ routes/inertia.php
@@ -20,7 +20,7 @@ Route::group(['middleware' => config('jetstream.middleware', ['web'])], function
         Route::get('/privacy-policy', [PrivacyPolicyController::class, 'show'])->name('policy.show');
     }
 
-    Route::group(['middleware' => ['auth', 'verified']], function () {
+    Route::group(['middleware' => ['auth:sanctum', 'verified']], function () {
         // User & Profile...
         Route::get('/user/profile', [UserProfileController::class, 'show'])
                     ->name('profile.show');
