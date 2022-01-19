fec0baea55a8055dd686baaf00076383b671b5b1
Julius Kiekbusch <jubeki99@gmail.com>
2022-01-19 16:46:31 +0100

    Add Check before deleting a profile photo (#959)
    * Add Check before deleting a profile photo
    
    * Formatting
    
    * Update HasProfilePhoto.php
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/HasProfilePhoto.php
+++ src/HasProfilePhoto.php
@@ -41,6 +41,10 @@ trait HasProfilePhoto
             return;
         }
 
+        if (is_null($this->profile_photo_path)) {
+            return;
+        }
+
         Storage::disk($this->profilePhotoDisk())->delete($this->profile_photo_path);
 
         $this->forceFill([
