20dab7ff4ea765a9af293f1d428f48622b15e4b2
Ahmed AlQahtani <alq.ahmad@gmail.com>
2022-03-12 17:02:29 +0300

    make checkbox component response to @checked(old()) syntax

--- resources/views/components/checkbox.blade.php
+++ resources/views/components/checkbox.blade.php
@@ -1,1 +1,3 @@
-<input type="checkbox" {!! $attributes->merge(['class' => 'rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50']) !!}>
+@props(['old' => false])
+
+<input type="checkbox" @checked(old($old)) {!! $attributes->merge(['class' => 'rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50']) !!}>
