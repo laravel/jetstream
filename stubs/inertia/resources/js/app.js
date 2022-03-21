f130ece1f4248f806b3997b713a390e52f8ba537
Wailan Tirajoh <wailantirajoh@gmail.com>
2022-03-21 23:02:49 +0700

    Replace dot with slash on name

--- stubs/inertia/resources/js/app.js
+++ stubs/inertia/resources/js/app.js
@@ -8,7 +8,7 @@ const appName = window.document.getElementsByTagName('title')[0]?.innerText || '
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => require(`./Pages/${name}.vue`),
+    resolve: (name) => require(`./Pages/${name.replace(".", "/")}.vue`),
     setup({ el, app, props, plugin }) {
         return createApp({ render: () => h(app, props) })
             .use(plugin)
