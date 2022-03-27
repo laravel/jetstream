45e8e1dbc6398c3ab5db61dba98e058bd7cb0cec
Sourav Rakshit <developer.srv1@gmail.com>
2022-03-27 16:01:48 +0530

    prop to prevent closing dropdown on item click

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -1,4 +1,4 @@
-@props(['align' => 'right', 'width' => '48', 'contentClasses' => 'py-1 bg-white', 'dropdownClasses' => ''])
+@props(['align' => 'right', 'width' => '48', 'contentClasses' => 'py-1 bg-white', 'dropdownClasses' => '', 'closeOnClick' => true])
 
 @php
 switch ($align) {
@@ -38,8 +38,8 @@ switch ($width) {
             x-transition:leave-start="transform opacity-100 scale-100"
             x-transition:leave-end="transform opacity-0 scale-95"
             class="absolute z-50 mt-2 {{ $width }} rounded-md shadow-lg {{ $alignmentClasses }} {{ $dropdownClasses }}"
-            style="display: none;"
-            @click="open = false">
+            {!! $closeOnClick ? 'x-on:click="open = false"' : '' !!}
+            style="display: none;">
         <div class="rounded-md ring-1 ring-black ring-opacity-5 {{ $contentClasses }}">
             {{ $content }}
         </div>
