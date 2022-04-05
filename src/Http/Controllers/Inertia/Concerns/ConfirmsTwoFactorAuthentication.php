692a7ab0c345575f94a38a489311609d6326cf2d
Manuel Leiner <manuel.leiner@naix.de>
2022-04-05 11:28:03 +0200

    Prevent user reset when not yet finished confirming

--- src/Http/Controllers/Inertia/Concerns/ConfirmsTwoFactorAuthentication.php
+++ src/Http/Controllers/Inertia/Concerns/ConfirmsTwoFactorAuthentication.php
@@ -77,7 +77,8 @@ trait ConfirmsTwoFactorAuthentication
      */
     protected function neverFinishedConfirmingTwoFactorAuthentication(Request $request, $currentTime)
     {
-        return is_null($request->user()->two_factor_confirmed_at) &&
+        return ! array_key_exists('code', $request->session()->getOldInput()) &&
+            is_null($request->user()->two_factor_confirmed_at) &&
             $request->session()->get('two_factor_confirming_at', 0) != $currentTime;
     }
 }
