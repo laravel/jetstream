a1633434d9105dfa09cda067d188514f87103a63
Jess Archer <jess@jessarcher.com>
2022-05-30 16:41:48 +1000

    Remove redundant dependency

--- stubs/inertia/webpack.mix.js
+++ stubs/inertia/webpack.mix.js
@@ -13,7 +13,6 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js').vue()
     .postCss('resources/css/app.css', 'public/css', [
-        require('postcss-import'),
         require('tailwindcss'),
     ])
     .alias({
