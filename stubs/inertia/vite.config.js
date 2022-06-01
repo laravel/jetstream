079cf40f3503fff10deaec68e32d26d18b9f40cf
Jess Archer <jess@jessarcher.com>
2022-06-01 17:45:47 +1000

    Use Vite

---
+++ stubs/inertia/vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+    plugins: [
+        laravel(),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+})
--- stubs/livewire/resources/js/app.js
+++ stubs/livewire/resources/js/app.js
@@ -1,4 +1,5 @@
 import './bootstrap';
+import '../css/app.css';
 
 import Alpine from 'alpinejs';
 
