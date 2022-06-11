5abf3f08e88548b31347d09b75925a928ff118f0
Samuel Mwangi <samuel@samuelmwangi.co.ke>
2022-06-11 19:49:07 +0300

    Add .vue extension to imports
    This add the .vue extension to match all other Vue files in the repository. 
    It also plays nice with Vite.

--- stubs/inertia/resources/js/Pages/Teams/Partials/UpdateTeamNameForm.vue
+++ stubs/inertia/resources/js/Pages/Teams/Partials/UpdateTeamNameForm.vue
@@ -1,11 +1,11 @@
 <script setup>
 import { useForm } from '@inertiajs/inertia-vue3';
-import JetActionMessage from '@/Jetstream/ActionMessage';
-import JetButton from '@/Jetstream/Button';
-import JetFormSection from '@/Jetstream/FormSection';
-import JetInput from '@/Jetstream/Input';
-import JetInputError from '@/Jetstream/InputError';
-import JetLabel from '@/Jetstream/Label';
+import JetActionMessage from '@/Jetstream/ActionMessage.vue';
+import JetButton from '@/Jetstream/Button.vue';
+import JetFormSection from '@/Jetstream/FormSection.vue';
+import JetInput from '@/Jetstream/Input.vue';
+import JetInputError from '@/Jetstream/InputError.vue';
+import JetLabel from '@/Jetstream/Label.vue';
 
 const props = defineProps({
     team: Object,
