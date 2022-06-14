9ea9447669c83a2d77a2bf382d360059ae536adf
prestonholt <prestonholt@me.com>
2022-06-14 13:11:47 -0400

    Fix quotations

--- stubs/inertia/resources/js/app.js
+++ stubs/inertia/resources/js/app.js
@@ -2,7 +2,7 @@ import './bootstrap';
 
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/inertia-vue3';
-import { ZiggyVue } from "ziggy";
+import { ZiggyVue } from 'ziggy';
 import { InertiaProgress } from '@inertiajs/progress';
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
