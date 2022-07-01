9b4226d465e9197731a969256a47c887bdf54104
Tim MacDonald <hello@timacdonald.me>
2022-07-02 06:42:20 +1000

    add default reloading to jestream stacks (#1080)

--- stubs/inertia/vite.config.js
+++ stubs/inertia/vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: 'resources/js/app.js',
+            refresh: true,
         }),
         vue({
             template: {
