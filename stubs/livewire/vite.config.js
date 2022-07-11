ee23dac932fa3344d3a7939b7a2a8fda10e57a53
Tim MacDonald <hello@timacdonald.me>
2022-07-11 23:13:54 +1000

    add livewire component paths to default refresh paths for livewire (#1081)

--- stubs/livewire/vite.config.js
+++ stubs/livewire/vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import laravel, { refreshPaths } from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
@@ -8,7 +8,10 @@ export default defineConfig({
                 'resources/css/app.css',
                 'resources/js/app.js',
             ],
-            refresh: true,
+            refresh: [
+                ...refreshPaths,
+                'app/Http/Livewire/**',
+            ],
         }),
     ],
 });
