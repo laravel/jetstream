e10ec234f612ed3c0f60c6ea6d303461e2c94ae9
Taylor Otwell <taylor@laravel.com>
2022-07-20 07:48:35 -0500

    Update app.blade.php

--- stubs/livewire/resources/views/layouts/app.blade.php
+++ stubs/livewire/resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
             <!-- Page Heading -->
             @if (isset($header))
                 <header class="bg-white shadow">
-                    <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
+                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
                 </header>
