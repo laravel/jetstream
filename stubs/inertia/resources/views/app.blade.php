24fa52370f5932339464302c76ef8873655bfa69
Lucas Rolff <lucas@lucasrolff.com>
2022-08-01 08:53:35 +0200

    Switch to fonts.bunny.net instead of Google Fonts

--- stubs/inertia/resources/views/app.blade.php
+++ stubs/inertia/resources/views/app.blade.php
@@ -7,7 +7,7 @@
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Scripts -->
         @routes
--- stubs/livewire/resources/views/layouts/guest.blade.php
+++ stubs/livewire/resources/views/layouts/guest.blade.php
@@ -8,7 +8,7 @@
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
