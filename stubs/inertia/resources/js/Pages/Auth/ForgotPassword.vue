2dfabd07c09ff3bb4405bbb40761c679972b32a6
Dale Weaver <dalew@missionconnect.me>
2022-08-09 15:50:08 -0400

    Display validation errors alongside the fields

--- stubs/inertia/resources/js/Pages/Auth/ForgotPassword.vue
+++ stubs/inertia/resources/js/Pages/Auth/ForgotPassword.vue
@@ -4,8 +4,8 @@ import JetAuthenticationCard from '@/Components/AuthenticationCard.vue';
 import JetAuthenticationCardLogo from '@/Components/AuthenticationCardLogo.vue';
 import JetButton from '@/Components/Button.vue';
 import JetInput from '@/Components/Input.vue';
+import JetInputError from '@/Components/InputError.vue';
 import JetLabel from '@/Components/Label.vue';
-import JetValidationErrors from '@/Components/ValidationErrors.vue';
 
 defineProps({
     status: String,
@@ -36,25 +36,15 @@ const submit = () => {
             {{ status }}
         </div>
 
-        <JetValidationErrors class="mb-4" />
-
         <form @submit.prevent="submit">
             <div>
                 <JetLabel for="email" value="Email" />
-                <JetInput
-                    id="email"
-                    v-model="form.email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    required
-                    autofocus
-                />
+                <JetInput id="email" v-model="form.email" type="email" class="mt-1 block w-full" required autofocus />
+                <JetInputError class="mt-2" :message="form.errors.email" />
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                <JetButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Email Password Reset Link
-                </JetButton>
+                <JetButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing"> Email Password Reset Link </JetButton>
             </div>
         </form>
     </JetAuthenticationCard>
--- stubs/inertia/resources/js/Pages/Auth/TwoFactorChallenge.vue
+++ stubs/inertia/resources/js/Pages/Auth/TwoFactorChallenge.vue
@@ -5,8 +5,8 @@ import JetAuthenticationCard from '@/Components/AuthenticationCard.vue';
 import JetAuthenticationCardLogo from '@/Components/AuthenticationCardLogo.vue';
 import JetButton from '@/Components/Button.vue';
 import JetInput from '@/Components/Input.vue';
+import JetInputError from '@/Components/InputError.vue';
 import JetLabel from '@/Components/Label.vue';
-import JetValidationErrors from '@/Components/ValidationErrors.vue';
 
 const recovery = ref(false);
 
@@ -46,58 +46,32 @@ const submit = () => {
         </template>
 
         <div class="mb-4 text-sm text-gray-600">
-            <template v-if="! recovery">
-                Please confirm access to your account by entering the authentication code provided by your authenticator application.
-            </template>
+            <template v-if="!recovery"> Please confirm access to your account by entering the authentication code provided by your authenticator application. </template>
 
-            <template v-else>
-                Please confirm access to your account by entering one of your emergency recovery codes.
-            </template>
+            <template v-else> Please confirm access to your account by entering one of your emergency recovery codes. </template>
         </div>
 
-        <JetValidationErrors class="mb-4" />
-
         <form @submit.prevent="submit">
-            <div v-if="! recovery">
+            <div v-if="!recovery">
                 <JetLabel for="code" value="Code" />
-                <JetInput
-                    id="code"
-                    ref="codeInput"
-                    v-model="form.code"
-                    type="text"
-                    inputmode="numeric"
-                    class="mt-1 block w-full"
-                    autofocus
-                    autocomplete="one-time-code"
-                />
+                <JetInput id="code" ref="codeInput" v-model="form.code" type="text" inputmode="numeric" class="mt-1 block w-full" autofocus autocomplete="one-time-code" />
+                <JetInputError class="mt-2" :message="form.errors.code" />
             </div>
 
             <div v-else>
                 <JetLabel for="recovery_code" value="Recovery Code" />
-                <JetInput
-                    id="recovery_code"
-                    ref="recoveryCodeInput"
-                    v-model="form.recovery_code"
-                    type="text"
-                    class="mt-1 block w-full"
-                    autocomplete="one-time-code"
-                />
+                <JetInput id="recovery_code" ref="recoveryCodeInput" v-model="form.recovery_code" type="text" class="mt-1 block w-full" autocomplete="one-time-code" />
+                <JetInputError class="mt-2" :message="form.errors.code" />
             </div>
 
             <div class="flex items-center justify-end mt-4">
                 <button type="button" class="text-sm text-gray-600 hover:text-gray-900 underline cursor-pointer" @click.prevent="toggleRecovery">
-                    <template v-if="! recovery">
-                        Use a recovery code
-                    </template>
+                    <template v-if="!recovery"> Use a recovery code </template>
 
-                    <template v-else>
-                        Use an authentication code
-                    </template>
+                    <template v-else> Use an authentication code </template>
                 </button>
 
-                <JetButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Log in
-                </JetButton>
+                <JetButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing"> Log in </JetButton>
             </div>
         </form>
     </JetAuthenticationCard>
