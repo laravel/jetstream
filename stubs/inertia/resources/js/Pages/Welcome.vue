c98fa0e0dc0a63822e7d54ee562b7e1e3c68d0ac
Jess Archer <jess@jessarcher.com>
2022-08-10 09:45:28 +1000

    Add missing dark variant for welcome nav links

--- stubs/inertia/resources/js/Pages/Welcome.vue
+++ stubs/inertia/resources/js/Pages/Welcome.vue
@@ -14,16 +14,16 @@ defineProps({
 
     <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
         <div v-if="canLogin" class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-            <Link v-if="$page.props.user" :href="route('dashboard')" class="text-sm text-gray-700 underline">
+            <Link v-if="$page.props.user" :href="route('dashboard')" class="text-sm text-gray-700 dark:text-gray-500 underline">
                 Dashboard
             </Link>
 
             <template v-else>
-                <Link :href="route('login')" class="text-sm text-gray-700 underline">
+                <Link :href="route('login')" class="text-sm text-gray-700 dark:text-gray-500 underline">
                     Log in
                 </Link>
 
-                <Link v-if="canRegister" :href="route('register')" class="ml-4 text-sm text-gray-700 underline">
+                <Link v-if="canRegister" :href="route('register')" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">
                     Register
                 </Link>
             </template>
