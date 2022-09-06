7b5f7bd86c7e0af362b0112f954f9c6955752b84
Tim MacDonald <hello@timacdonald.me>
2022-09-06 17:13:15 +1000

    no need to eager glob ssr

--- stubs/inertia/resources/js/ssr.js
+++ stubs/inertia/resources/js/ssr.js
@@ -12,7 +12,7 @@ createServer((page) =>
         page,
         render: renderToString,
         title: (title) => `${title} - ${appName}`,
-        resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue', { eager: true })),
+        resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
         setup({ app, props, plugin }) {
             return createSSRApp({ render: () => h(app, props) })
                 .use(plugin)
