aa973987b4ca57f74e93ee007675ed99b36fd08b
Mark Jaquith <mark@jaquith.me>
2022-10-21 13:57:56 -0400

    Include all user table columns in the UserFactory
    Without these fields in the factory, the default Jetstream tests will
    fail when using the Model::preventAccessingMissingAttributes() feature.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -30,6 +30,10 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'profile_photo_path' => null,
+            'two_factor_secret' => null,
+            'two_factor_confirmed_at' => null,
+            'two_factor_recovery_codes' => null,
         ];
     }
 
