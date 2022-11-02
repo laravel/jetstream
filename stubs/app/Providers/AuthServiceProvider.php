f14d52719c2211e88c15b17bbda19e93869bb6cf
Nuno Maduro <enunomaduro@gmail.com>
2022-11-02 13:06:20 +0000

    Minor changes on auth service provider

--- stubs/app/Providers/AuthServiceProvider.php
+++ stubs/app/Providers/AuthServiceProvider.php
@@ -11,7 +11,7 @@ class AuthServiceProvider extends ServiceProvider
     /**
      * The policy mappings for the application.
      *
-     * @var array
+     * @var array<class-string, class-string>
      */
     protected $policies = [
         Team::class => TeamPolicy::class,
