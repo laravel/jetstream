607eb6ecf3520a6db59ba228d0d5fe92cedb3b59
Nuno Maduro <enunomaduro@gmail.com>
2022-11-09 17:30:23 +0000

    Improves typing on removes team members

--- src/Contracts/RemovesTeamMembers.php
+++ src/Contracts/RemovesTeamMembers.php
@@ -3,7 +3,7 @@
 namespace Laravel\Jetstream\Contracts;
 
 /**
- * @method void remove(\Illuminate\Foundation\Auth\User $user, \Illuminate\Database\Eloquent\Model $team, \Illuminate\Database\Eloquent\Model $teamMember)
+ * @method void remove(\Illuminate\Foundation\Auth\User $user, \Illuminate\Database\Eloquent\Model $team, \Illuminate\Foundation\Auth\User $teamMember)
  */
 interface RemovesTeamMembers
 {
