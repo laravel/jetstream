0456f71128cc00f3638e97714615c49be874ef18
Ryan <ryan@x7media.com>
2022-12-01 13:38:35 -0800

    Style fix

--- stubs/livewire/resources/js/app.js
+++ stubs/livewire/resources/js/app.js
@@ -1,10 +1,10 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
-import focus from '@alpinejs/focus'
+import focus from '@alpinejs/focus';
  
 window.Alpine = Alpine;
 
-Alpine.plugin(focus)
+Alpine.plugin(focus);
 
 Alpine.start();
