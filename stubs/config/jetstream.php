52036b913786cfb1210196fa42601914c0c296c6
Abdurashid Abdumominov <78313030+abdurashid-dev@users.noreply.github.com>
2022-12-20 11:54:16 +0500

    Update jetstream.php

--- stubs/config/jetstream.php
+++ stubs/config/jetstream.php
@@ -53,7 +53,7 @@ return [
     |
     | Some of Jetstream's features are optional. You may disable the features
     | by removing them from this array. You're free to only remove some of
-    | these features or you can even remove all of these if you need to.
+    | these features, or you can even remove all of these if you need to.
     |
     */
 
