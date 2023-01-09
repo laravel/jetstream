9aaedb51109b0558ea6df187de4909e8e8c1969d
Andrew Ponomarev <spargo32@gmail.com>
2023-01-09 08:56:15 +0300

    Make section title full width

--- stubs/inertia/resources/js/Components/SectionTitle.vue
+++ stubs/inertia/resources/js/Components/SectionTitle.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="md:col-span-1 flex justify-between">
-        <div class="px-4 sm:px-0">
+        <div class="flex-grow px-4 sm:px-0">
             <h3 class="text-lg font-medium text-gray-900">
                 <slot name="title" />
             </h3>
