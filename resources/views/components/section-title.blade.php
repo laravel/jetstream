54d48b199c418bf2ef041aa8e441b892877f6917
Andrew Ponomarev <spargo32@gmail.com>
2023-01-09 14:03:36 +0300

    Fix class name to Tailwind CSS v3

--- resources/views/components/section-title.blade.php
+++ resources/views/components/section-title.blade.php
@@ -1,5 +1,5 @@
 <div class="md:col-span-1 flex justify-between">
-    <div class="flex-grow px-4 sm:px-0">
+    <div class="grow px-4 sm:px-0">
         <h3 class="text-lg font-medium text-gray-900">{{ $title }}</h3>
 
         <p class="mt-1 text-sm text-gray-600">
