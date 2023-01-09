4a51644434d89df7d5f17dbcc6ae8026349e5925
Jess Archer <jess@jessarcher.com>
2023-01-09 15:32:16 +1000

    Prevent `MissingAttributeException`

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,7 +29,11 @@ class UserFactory extends Factory
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'two_factor_secret' => null,
+            'two_factor_recovery_codes' => null,
             'remember_token' => Str::random(10),
+            'profile_photo_path' => null,
+            'current_team_id' => null,
         ];
     }
 
