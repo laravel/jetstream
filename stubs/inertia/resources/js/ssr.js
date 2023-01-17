883ac64e542d27254d44504f87ac11520d19744b
Jess Archer <jess@jessarcher.com>
2023-01-17 10:50:34 +1000

    Fix SSR build

--- stubs/inertia/resources/js/ssr.js
+++ stubs/inertia/resources/js/ssr.js
@@ -1,7 +1,7 @@
 import { createSSRApp, h } from 'vue';
 import { renderToString } from '@vue/server-renderer';
 import { createInertiaApp } from '@inertiajs/vue3';
-import createServer from '@inertiajs/server';
+import createServer from '@inertiajs/vue3/server';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 
