c7af44fd6fc41b98edd1e952b1bfbad27d45fb25
Nicolas Hedger <649677+nhedger@users.noreply.github.com>
2023-01-22 13:18:15 +0100

    Fix typo

--- stubs/inertia/resources/js/Components/Welcome.vue
+++ stubs/inertia/resources/js/Components/Welcome.vue
@@ -101,7 +101,7 @@ import ApplicationLogo from '@/Components/ApplicationLogo.vue';
 
                 <div class="ml-12">
                     <div class="mt-2 text-sm text-gray-500">
-                        Authentication and registration views are included with Laravel Jetstream, as well as support for user email verification and resetting forgotten passwords. So, you're free to get started what matters most: building your application.
+                        Authentication and registration views are included with Laravel Jetstream, as well as support for user email verification and resetting forgotten passwords. So, you're free to get started with what matters most: building your application.
                     </div>
                 </div>
             </div>
