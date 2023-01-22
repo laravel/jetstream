92d6ef46d439cf6a653ba27fa41c6e126c012564
Nicolas Hedger <649677+nhedger@users.noreply.github.com>
2023-01-22 18:33:28 +0100

    Fix typo (#1230)

--- resources/views/components/welcome.blade.php
+++ resources/views/components/welcome.blade.php
@@ -87,7 +87,7 @@
 
         <div class="ml-12">
             <div class="mt-2 text-sm text-gray-500">
-                Authentication and registration views are included with Laravel Jetstream, as well as support for user email verification and resetting forgotten passwords. So, you're free to get started what matters most: building your application.
+                Authentication and registration views are included with Laravel Jetstream, as well as support for user email verification and resetting forgotten passwords. So, you're free to get started with what matters most: building your application.
             </div>
         </div>
     </div>
