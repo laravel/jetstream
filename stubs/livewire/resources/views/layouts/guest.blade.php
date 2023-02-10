45a42c11baa86811650ffbc644040aab40ff433d
Jess Archer <jess@jessarcher.com>
2023-02-11 01:40:39 +1000

    [3.x] New welcome (#1247)
    * Update font and welcome page
    
    * Freshen welcome component
    
    * Tweak spacing and colors
    
    * Fix arrow colors

--- stubs/livewire/resources/views/layouts/guest.blade.php
+++ stubs/livewire/resources/views/layouts/guest.blade.php
@@ -8,7 +8,8 @@
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
--- stubs/livewire/tailwind.config.js
+++ stubs/livewire/tailwind.config.js
@@ -12,7 +12,7 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
         },
     },
