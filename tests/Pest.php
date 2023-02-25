f3963aaf3f1489ee43071493d2b996ef111b7f8f
Joel Butcher <joel167897@gmail.com>
2023-02-25 08:44:49 +0000

    install pest

---
+++ tests/Pest.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Test Case
+|--------------------------------------------------------------------------
+|
+| The closure you provide to your test functions is always bound to a specific PHPUnit test
+| case class. By default, that class is "PHPUnit\Framework\TestCase". Of course, you may
+| need to change it using the "uses()" function to bind a different classes or traits.
+|
+*/
+
+use Laravel\Jetstream\Tests\OrchestraTestCase;
+
+uses(OrchestraTestCase::class)->in('Feature', 'Unit');
