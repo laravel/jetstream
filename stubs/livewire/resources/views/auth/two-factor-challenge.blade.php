1d2d6d075c61abd2f3920c7eacc22cb8d3929b30
Andrew Wallo <andrewdwallo@gmail.com>
2023-04-05 14:38:29 -0400

    Fix issue with page reload/refresh on two-factor-challenge page (#1291)
    * fix: issue on two-factor-challenge page
    
    * formatting
    
    ---------
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- stubs/livewire/resources/views/auth/two-factor-challenge.blade.php
+++ stubs/livewire/resources/views/auth/two-factor-challenge.blade.php
@@ -5,11 +5,11 @@
         </x-slot>
 
         <div x-data="{ recovery: false }">
-            <div class="mb-4 text-sm text-gray-600 dark:text-gray-400" x-show="! recovery">
+            <div class="mb-4 text-sm text-gray-600 dark:text-gray-400" x-cloak x-show="! recovery">
                 {{ __('Please confirm access to your account by entering the authentication code provided by your authenticator application.') }}
             </div>
 
-            <div class="mb-4 text-sm text-gray-600 dark:text-gray-400" x-show="recovery">
+            <div class="mb-4 text-sm text-gray-600 dark:text-gray-400" x-cloak x-show="recovery">
                 {{ __('Please confirm access to your account by entering one of your emergency recovery codes.') }}
             </div>
 
@@ -18,18 +18,19 @@
             <form method="POST" action="{{ route('two-factor.login') }}">
                 @csrf
 
-                <div class="mt-4" x-show="! recovery">
+                <div class="mt-4" x-cloak x-show="! recovery">
                     <x-label for="code" value="{{ __('Code') }}" />
                     <x-input id="code" class="block mt-1 w-full" type="text" inputmode="numeric" name="code" autofocus x-ref="code" autocomplete="one-time-code" />
                 </div>
 
-                <div class="mt-4" x-show="recovery">
+                <div class="mt-4" x-cloak x-show="recovery">
                     <x-label for="recovery_code" value="{{ __('Recovery Code') }}" />
                     <x-input id="recovery_code" class="block mt-1 w-full" type="text" name="recovery_code" x-ref="recovery_code" autocomplete="one-time-code" />
                 </div>
 
                 <div class="flex items-center justify-end mt-4">
                     <button type="button" class="text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 underline cursor-pointer"
+                                    x-cloak
                                     x-show="! recovery"
                                     x-on:click="
                                         recovery = true;
@@ -39,6 +40,7 @@
                     </button>
 
                     <button type="button" class="text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 underline cursor-pointer"
+                                    x-cloak
                                     x-show="recovery"
                                     x-on:click="
                                         recovery = false;
