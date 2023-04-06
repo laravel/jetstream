8226596bdac3b3d6c6387aaf2bde2bb93e0dbf42
Taylor Otwell <taylor@laravel.com>
2023-04-06 10:14:07 -0500

    wip

--- stubs/livewire/resources/views/auth/two-factor-challenge.blade.php
+++ stubs/livewire/resources/views/auth/two-factor-challenge.blade.php
@@ -5,7 +5,7 @@
         </x-slot>
 
         <div x-data="{ recovery: false }">
-            <div class="mb-4 text-sm text-gray-600 dark:text-gray-400" x-cloak x-show="! recovery">
+            <div class="mb-4 text-sm text-gray-600 dark:text-gray-400" x-show="! recovery">
                 {{ __('Please confirm access to your account by entering the authentication code provided by your authenticator application.') }}
             </div>
 
@@ -18,7 +18,7 @@
             <form method="POST" action="{{ route('two-factor.login') }}">
                 @csrf
 
-                <div class="mt-4" x-cloak x-show="! recovery">
+                <div class="mt-4" x-show="! recovery">
                     <x-label for="code" value="{{ __('Code') }}" />
                     <x-input id="code" class="block mt-1 w-full" type="text" inputmode="numeric" name="code" autofocus x-ref="code" autocomplete="one-time-code" />
                 </div>
@@ -30,7 +30,6 @@
 
                 <div class="flex items-center justify-end mt-4">
                     <button type="button" class="text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 underline cursor-pointer"
-                                    x-cloak
                                     x-show="! recovery"
                                     x-on:click="
                                         recovery = true;
