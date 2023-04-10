173f5748b8be7a6fb808d8b36a94d2c00180a641
Taylor Otwell <taylor@laravel.com>
2023-04-10 09:04:02 -0500

    Update navigation-menu.blade.php

--- stubs/livewire/resources/views/navigation-menu.blade.php
+++ stubs/livewire/resources/views/navigation-menu.blade.php
@@ -54,7 +54,7 @@
                                     @endcan
 
                                     <!-- Team Switcher -->
-                                    @if(Auth::user()->allTeams()->count() > 1)
+                                    @if (Auth::user()->allTeams()->count() > 1)
                                         <div class="border-t border-gray-200 dark:border-gray-600"></div>
 
                                         <div class="block px-4 py-2 text-xs text-gray-400">
@@ -201,7 +201,7 @@
                     @endcan
 
                     <!-- Team Switcher -->
-                    @if(Auth::user()->allTeams()->count() > 1)
+                    @if (Auth::user()->allTeams()->count() > 1)
                         <div class="border-t border-gray-200 dark:border-gray-600"></div>
 
                         <div class="block px-4 py-2 text-xs text-gray-400">
