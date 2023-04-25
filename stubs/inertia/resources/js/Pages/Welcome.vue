c7f92333f6210b182b3f61ffe2785caceec50976
Ayman Atmeh <ayman.atmeh@gmail.com>
2023-04-25 10:47:56 +0300

    Update Welcome.vue
    "Set z-index to 10 for Login/Register container to ensure it appears on top of other elements"

--- stubs/inertia/resources/js/Pages/Welcome.vue
+++ stubs/inertia/resources/js/Pages/Welcome.vue
@@ -13,7 +13,7 @@ defineProps({
     <Head title="Welcome" />
 
     <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-        <div v-if="canLogin" class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
+        <div v-if="canLogin" class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
             <Link v-if="$page.props.auth.user" :href="route('dashboard')" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</Link>
 
             <template v-else>
