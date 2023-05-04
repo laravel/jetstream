e109cbd77693ef386c86c9d6f9e1e7104bf21b4a
Tim MacDonald <hello@timacdonald.me>
2023-05-05 01:02:46 +1000

    Migrate to modules (#1242)

--- stubs/livewire/tailwind.config.js
+++ stubs/livewire/tailwind.config.js
@@ -1,7 +1,9 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './vendor/laravel/jetstream/**/*.blade.php',
@@ -17,5 +19,5 @@ module.exports = {
         },
     },
 
-    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+    plugins: [forms, typography],
 };
