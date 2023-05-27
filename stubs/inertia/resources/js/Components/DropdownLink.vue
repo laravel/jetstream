c377802d47eea7b84257b874319376f9cd4d01a3
filippetrovicAT <filip@bitecorn.com>
2023-05-27 12:43:14 +0200

    added space in 'DropDownLink.vue' component

--- stubs/inertia/resources/js/Components/DropdownLink.vue
+++ stubs/inertia/resources/js/Components/DropdownLink.vue
@@ -13,7 +13,7 @@ defineProps({
             <slot />
         </button>
 
-        <a v-else-if="as =='a'" :href="href" class="block px-4 py-2 text-sm leading-5 text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-800 transition duration-150 ease-in-out">
+        <a v-else-if="as == 'a'" :href="href" class="block px-4 py-2 text-sm leading-5 text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-800 transition duration-150 ease-in-out">
             <slot />
         </a>
 
