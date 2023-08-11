1e27d59cf5e8d63e05abf32122455a1db024bf42
Nuno Maduro <enunomaduro@gmail.com>
2023-08-11 14:42:08 +0100

    Improves Jetstream model factories

--- database/factories/TeamFactory.php
+++ database/factories/TeamFactory.php
@@ -6,15 +6,11 @@ use App\Models\Team;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Team>
+ */
 class TeamFactory extends Factory
 {
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = Team::class;
-
     /**
      * Define the model's default state.
      *
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -8,15 +8,11 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 use Laravel\Jetstream\Features;
 
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
 class UserFactory extends Factory
 {
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = User::class;
-
     /**
      * Define the model's default state.
      *
