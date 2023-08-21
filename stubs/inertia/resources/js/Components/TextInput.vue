d32c311a6ba02c5d04c1ed460bd3a4d3b46f59ef
Nuno Maduro <enunomaduro@gmail.com>
2023-08-21 15:08:59 +0100

    Revert `emit` change

--- stubs/inertia/resources/js/Components/TextInput.vue
+++ stubs/inertia/resources/js/Components/TextInput.vue
@@ -23,6 +23,6 @@ defineExpose({ focus: () => input.value.focus() });
         ref="input"
         class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm"
         :value="modelValue"
-        @input="$dispatch('update:modelValue', $event.target.value)"
+        @input="$emit('update:modelValue', $event.target.value)"
     >
 </template>
