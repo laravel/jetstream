5b445d2bc08b1ff7aa254617e5a6954bda937b97
Taylor Otwell <taylor@laravel.com>
2023-10-18 08:53:59 -0500

    wip

--- stubs/livewire/resources/views/profile/update-profile-information-form.blade.php
+++ stubs/livewire/resources/views/profile/update-profile-information-form.blade.php
@@ -12,7 +12,7 @@
         @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
             <div x-data="{photoName: null, photoPreview: null}" class="col-span-6 sm:col-span-4">
                 <!-- Profile Photo File Input -->
-                <input type="file" class="hidden"
+                <input type="file" id="photo" class="hidden"
                             wire:model.live="photo"
                             x-ref="photo"
                             x-on:change="
