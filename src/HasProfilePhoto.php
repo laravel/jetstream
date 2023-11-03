8807da2f7be51236524ccdd23deade375e1a348d
Potsky <potsky@me.com>
2023-11-03 11:17:23 +0100

    Typehint mutator profile_photo_url attribute function return
    We need to typehint mutator function return to let IDE Helper package know the type of the attribute.
    
    https://github.com/barryvdh/laravel-ide-helper/issues/1315#issuecomment-1688154721
    
    Then PHPStan will not throw error on undefined profile_photo_url property.

--- src/HasProfilePhoto.php
+++ src/HasProfilePhoto.php
@@ -59,7 +59,7 @@ trait HasProfilePhoto
      */
     public function profilePhotoUrl(): Attribute
     {
-        return Attribute::get(function () {
+        return Attribute::get(function (): string {
             return $this->profile_photo_path
                     ? Storage::disk($this->profilePhotoDisk())->url($this->profile_photo_path)
                     : $this->defaultProfilePhotoUrl();
