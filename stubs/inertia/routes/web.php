3fd9cab5a2b0d4b4c4ceccc1c59dc0a98be6ab87
Nuno Maduro <enunomaduro@gmail.com>
2023-11-27 17:40:01 +0000

    Comments

--- stubs/inertia/routes/web.php
+++ stubs/inertia/routes/web.php
@@ -10,8 +10,8 @@ use Inertia\Inertia;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded within the "web" middleware group which includes
+| sessions, cookie encryption, and more. Go build something great!
 |
 */
 
