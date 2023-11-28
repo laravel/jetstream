4c958b1b4cb0f965d95d8292b67ba152e1e14e6e
Nuno Maduro <enunomaduro@gmail.com>
2023-11-28 10:05:34 +0000

    Removes non-used imports

--- stubs/pest-tests/RegistrationTest.php
+++ stubs/pest-tests/RegistrationTest.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Providers\RouteServiceProvider;
 use Laravel\Fortify\Features;
 use Laravel\Jetstream\Jetstream;
 
--- stubs/tests/RegistrationTest.php
+++ stubs/tests/RegistrationTest.php
@@ -2,7 +2,6 @@
 
 namespace Tests\Feature;
 
-use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Laravel\Fortify\Features;
 use Laravel\Jetstream\Jetstream;
