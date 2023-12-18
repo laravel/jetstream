5846542d53807821c0a3a0ec0d0392f17a6b50ee
Tim MacDonald <hello@timacdonald.me>
2023-12-18 12:05:14 +1100

    Use default refresh paths

--- stubs/livewire/vite.config.js
+++ stubs/livewire/vite.config.js
@@ -8,10 +8,7 @@ export default defineConfig({
                 'resources/css/app.css',
                 'resources/js/app.js',
             ],
-            refresh: [
-                ...refreshPaths,
-                'app/Livewire/**',
-            ],
+            refresh: true,
         }),
     ],
 });
