4855a5b3155d0da4b465df65cb74530a3d272986
Nuno Maduro <enunomaduro@gmail.com>
2024-01-18 12:52:01 +0000

    Slim comments

--- stubs/inertia/routes/web.php
+++ stubs/inertia/routes/web.php
@@ -4,17 +4,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded within the "web" middleware group which includes
-| sessions, cookie encryption, and more. Go build something great!
-|
-*/
-
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
