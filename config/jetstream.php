92e0d41776b3337283f941f0cc25c2d209dd5eee
MHO <yannick@chillpills.io>
2024-03-15 15:06:13 +0100

    Add auth_session variable in config file for Laravel 11

--- config/jetstream.php
+++ config/jetstream.php
@@ -1,10 +1,12 @@
 <?php
 
 use Laravel\Jetstream\Features;
+use Laravel\Jetstream\Http\Middleware\AuthenticateSession;
 
 return [
     'stack' => 'inertia',
     'middleware' => ['web'],
     'features' => [Features::accountDeletion()],
     'profile_photo_disk' => 'public',
+    'auth_session' => AuthenticateSession::class,
 ];
