c6e501b4dfb838f6ef4ba646c35d8d521b9911cb
Angel Vejar-Cortes <primilink@gmail.com>
2024-04-24 11:48:25 -0600

    Update ProfileInformationTest.php
    Added feature verification

--- stubs/tests/inertia/ProfileInformationTest.php
+++ stubs/tests/inertia/ProfileInformationTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use Laravel\Fortify\Features;
 
 class ProfileInformationTest extends TestCase
 {
@@ -12,6 +13,10 @@ class ProfileInformationTest extends TestCase
 
     public function test_profile_information_can_be_updated(): void
     {
+        if (!Features::canUpdateProfileInformation()) {
+            $this->markTestSkipped('Profile information update is not enabled');
+        }
+        
         $this->actingAs($user = User::factory()->create());
 
         $response = $this->put('/user/profile-information', [
