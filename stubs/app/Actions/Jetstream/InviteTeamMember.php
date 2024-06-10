d26c72295176405013416d6a41737582d21c5353
nanato12 <admin@okj.info>
2024-06-10 11:24:44 +0900

    fix: return type InviteTeamMember.php

--- stubs/app/Actions/Jetstream/InviteTeamMember.php
+++ stubs/app/Actions/Jetstream/InviteTeamMember.php
@@ -55,7 +55,7 @@ class InviteTeamMember implements InvitesTeamMembers
     /**
      * Get the validation rules for inviting a team member.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     * @return array<string, null|array<\Illuminate\Contracts\Validation\Rule|string>>
      */
     protected function rules(Team $team): array
     {
