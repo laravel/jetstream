c5ea645c62394862b1d4fabe92b11db4608751ba
Mauro Baptista <mauro.baptista@carnou.com>
2025-11-04 15:59:44 -0500

    Query team_id on delete

--- src/Http/Livewire/TeamMemberManager.php
+++ src/Http/Livewire/TeamMemberManager.php
@@ -130,7 +130,9 @@ class TeamMemberManager extends Component
         if (! empty($invitationId)) {
             $model = Jetstream::teamInvitationModel();
 
-            $model::whereKey($invitationId)->delete();
+            $model::whereKey($invitationId)
+                ->where('team_id', $team->id)
+                ->delete();
         }
 
         $this->team = $this->team->fresh();
